88bea7b6f12afbffb05bba3d3b75586250060bfa
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 15:02:54 -0700

    Update ad_bugs.sql

--- ad_bugs.sql
+++ ad_bugs.sql
@@ -1,3 +1,5 @@
 set lines 230
 col APPLICATION_SHORT_NAME for a6 heading ASN
-select * from apps.ad_bugs where bug_number='&bug_number';
+select  * 
+from    apps.ad_bugs 
+where   bug_number='&bug_number';
