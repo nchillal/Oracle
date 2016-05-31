c0f8b879bcd1fec93cb6619db8daad2a8a0487f8
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 15:13:04 -0700

    Update getdblink_details.sql

--- getdblink_details.sql
+++ getdblink_details.sql
@@ -4,5 +4,6 @@ COLUMN owner FORMAT a20
 COLUMN username FORMAT a20
 COLUMN host FORMAT a40
 
-SELECT * FROM dba_db_links 
-WHERE db_link like '&db_link';
+SELECT  * 
+FROM    dba_db_links 
+WHERE   db_link like '&db_link';
