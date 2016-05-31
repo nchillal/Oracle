ec6685537ee7d6a982a1277f0d78fdf7eab3028b
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 15:03:24 -0700

    Update arcs_required_for_resetlogs.sql

--- arcs_required_for_resetlogs.sql
+++ arcs_required_for_resetlogs.sql
@@ -1,2 +1,7 @@
 set numwidth 30 lines 230
-select thread#, sequence#, first_change#, next_change# from v$archived_log where &change between first_change# and next_change#;
+select  thread#, 
+        sequence#, 
+        first_change#, 
+        next_change# 
+from    v$archived_log 
+where   &change between first_change# and next_change#;
