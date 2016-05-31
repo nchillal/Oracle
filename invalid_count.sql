f9879595d58cbbac90c89254cb207745629c7c6d
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 15:13:58 -0700

    Update invalid_count.sql

--- invalid_count.sql
+++ invalid_count.sql
@@ -1,2 +1,7 @@
 set lines 230 pages 200
-select owner, count(*) from dba_objects where status='INVALID' group by owner order by 2 desc;
+select    owner, 
+          count(*) 
+from      dba_objects 
+where     status='INVALID' 
+group by  owner 
+order by  2 desc;
