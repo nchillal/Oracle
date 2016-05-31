9784fe0f93adca916db1a3fbc82253b1d959d27d
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 15:06:14 -0700

    Update conc_program_run_history.sql

--- conc_program_run_history.sql
+++ conc_program_run_history.sql
@@ -4,16 +4,13 @@ col logfile_name format a55 word_wrap
 col argument_text format a30
 col "Total Run Time" for a30
 
-select  	request_id,
-        	p.user_concurrent_program_name progName,
+select    request_id,
+          p.user_concurrent_program_name progName,
         	argument_text,
         	r.logfile_name,
-        	EXTRACT(DAY FROM (actual_completion_date - actual_start_date) DAY TO SECOND )
-  			|| ' days '
-  			|| EXTRACT(HOUR FROM (actual_completion_date - actual_start_date) DAY TO SECOND )
-  			|| ' hours '
-  			|| EXTRACT(MINUTE FROM (actual_completion_date - actual_start_date) DAY TO SECOND )
-  			|| ' minutes' "Total Run Time"
+        	EXTRACT(DAY FROM (actual_completion_date - actual_start_date) DAY TO SECOND ) || ' days ' || 
+        	EXTRACT(HOUR FROM (actual_completion_date - actual_start_date) DAY TO SECOND ) || ' hours ' || 
+        	EXTRACT(MINUTE FROM (actual_completion_date - actual_start_date) DAY TO SECOND ) || ' minutes' "Total Run Time"
 FROM    	applsys.fnd_concurrent_requests r, apps.fnd_concurrent_programs_tl p
 WHERE   	p.concurrent_program_id = r.concurrent_program_id
 and     	r.phase_code='C'
