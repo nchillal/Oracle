9fb20b08096bbdf34ff0601fced4bea3c0d10264
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 14:21:28 -0700

    Create create_baseline.sql
    Create baseline based on start & end snapshot id

---
+++ create_baseline.sql
@@ -0,0 +1,9 @@
+# Purpose: Create baseline based on start & end snapshot id. 
+
+BEGIN
+  DBMS_WORKLOAD_REPOSITORY.create_baseline (
+    start_snap_id => &start_snapid, 
+    end_snap_id   => &end_snapid,
+    baseline_name => '&baseline_name');
+END;
+/
