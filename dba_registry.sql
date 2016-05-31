28d12025b5cf56073635129e4010f72466fdbfae
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 15:11:09 -0700

    Update dba_registry.sql

--- dba_registry.sql
+++ dba_registry.sql
@@ -1,4 +1,8 @@
 set lines 230 pages 30
 col comp_name for a80
-select comp_id, comp_name, version, status from dba_registry
+select    comp_id, 
+          comp_name, 
+          version, 
+          status 
+from dba_registry
 /
