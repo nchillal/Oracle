3aaadb99629fb1200236b8903d1fa1515cda5194
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 23:16:42 -0700

    Update standby_log.sql

--- standby_log.sql
+++ standby_log.sql
@@ -1,5 +1,16 @@
-select inst_id, thread#, GROUP#, bytes/1024/1024, status from gv$standby_log;
+select  inst_id, 
+        thread#, 
+        GROUP#, 
+        bytes/1024/1024, 
+        status 
+from gv$standby_log;
 
 set lines 230 pages 200
 col member for a70
-select inst_id, group#,  member, status from gv$logfile where type='STANDBY';
+
+select  inst_id, 
+        group#,  
+        member, 
+        status 
+from    gv$logfile 
+where   type = 'STANDBY';
