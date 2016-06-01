171125ef9ba02483e369bd224817b09daa4a0381
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 23:13:55 -0700

    Update dbfile_mp.sql

--- dbfile_mp.sql
+++ dbfile_mp.sql
@@ -1,7 +1,11 @@
-SELECT SUBSTR(name, 1, (INSTR(name, '/', -1)-1)) FROM v$datafile
+SELECT  SUBSTR(name, 1, (INSTR(name, '/', -1)-1)) 
+FROM    v$datafile
 UNION
-SELECT SUBSTR(member, 1, (INSTR(member, '/', -1)-1)) FROM v$logfile
+SELECT  SUBSTR(member, 1, (INSTR(member, '/', -1)-1)) 
+FROM    v$logfile
 UNION
-SELECT SUBSTR(name, 1, (INSTR(name, '/', -1)-1)) FROM v$controlfile
+SELECT  SUBSTR(name, 1, (INSTR(name, '/', -1)-1)) 
+FROM    v$controlfile
 UNION
-SELECT SUBSTR(name, 1, (INSTR(name, '/', -1)-1)) FROM v$tempfile;
+SELECT  SUBSTR(name, 1, (INSTR(name, '/', -1)-1)) 
+FROM    v$tempfile;
