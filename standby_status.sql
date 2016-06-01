c534389bc07b59ce65fd25677f3ca8f728e0a646
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 23:15:46 -0700

    Update standby_status.sql

--- standby_status.sql
+++ standby_status.sql
@@ -1,3 +1,9 @@
 set lines 230 
 col dest_name for a30
-select dest_id, dest_name, status, error from V$ARCHIVE_DEST_STATUS where dest_id < 3;
+
+select  dest_id, 
+        dest_name, 
+        status, 
+        error 
+from    V$ARCHIVE_DEST_STATUS 
+where   dest_id < 3;
