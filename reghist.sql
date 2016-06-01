9925d47f2bef2de5e224abe6f15042a2c38da87b
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 23:24:28 -0700

    Update reghist.sql

--- reghist.sql
+++ reghist.sql
@@ -1,11 +1,13 @@
 set lines 230 
+
 col ACTION_TIME for a40
 col id for a10
 col comments for a30 
-select substr(action_time,1,30) action_time, 
-substr(id,1,10) id, 
-substr(action,1,10) action, 
-substr(version,1,8) version, 
-substr(BUNDLE_SERIES,1,6) bundle, 
-substr(comments,1,20) comments 
-from registry$history;
+
+select  SUBSTR(action_time,1,30) action_time, 
+        SUBSTR(id,1,10) id, 
+        SUBSTR(action,1,10) action, 
+        SUBSTR(version,1,8) version, 
+        SUBSTR(BUNDLE_SERIES,1,6) bundle, 
+        SUBSTR(comments,1,20) comments 
+from    registry$history;
