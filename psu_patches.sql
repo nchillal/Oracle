ccfa16b4ec194867057c448abd9791c3d08bb0bb
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 23:10:34 -0700

    Update psu_patches.sql

--- psu_patches.sql
+++ psu_patches.sql
@@ -1,5 +1,14 @@
 set lines 230 pages 50
+
 col action_time for a30 
 col comments for a50
 col bundle for a6
-select substr(action_time,1,30) action_time,substr(id,1,10) id,substr(action,1,10) action,substr(version,1,8) version,substr(BUNDLE_SERIES,1,6) bundle,substr(comments,1,20) comments from registry$history; 
+
+select  SUBSTR(action_time,1,30) 
+        action_time,
+        SUBSTR(id,1,10) id,
+        SUBSTR(action,1,10) action,
+        SUBSTR(version,1,8) version,
+        SUBSTR(BUNDLE_SERIES,1,6) bundle,
+        SUBSTR(comments,1,20) comments
+from    registry$history; 
