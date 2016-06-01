af11646bda8386c2530e9152dc2e5b90bf3f07ab
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 23:08:54 -0700

    Update logfile_switch.sql

--- logfile_switch.sql
+++ logfile_switch.sql
@@ -3,7 +3,9 @@ declare
 	v_arch_log_idle_mins_threshold number:=25;
 	v_arch_log_idle_mins number; 
 begin 
-	SELECT (sysdate-COMPLETION_TIME)*24*60 INTO v_arch_log_idle_mins FROM v$archived_log WHERE sequence#=(SELECT max(sequence#) FROM v$log_history) AND dest_id=1;
+	SELECT 	(SYSDATE-COMPLETION_TIME)*24*60 INTO v_arch_log_idle_mins 
+	FROM 		v$archived_log 
+	WHERE 	sequence# = (SELECT max(sequence#) FROM v$log_history) AND dest_id=1;
 
 	IF v_arch_log_idle_mins > v_arch_log_idle_mins_threshold THEN
 		DBMS_OUTPUT.PUT_LINE(CHR(10)||'Archive log idle minutes is '||v_arch_log_idle_mins||' which is above threshold '||v_arch_log_idle_mins_threshold);
