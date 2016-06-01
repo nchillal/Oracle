32e6382b5fe8c36e20571289ae4262a3bdda559b
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-06-01 11:27:56 -0700

    Update index_usage.sql

--- index_usage.sql
+++ index_usage.sql
@@ -1,6 +1,4 @@
 -- -----------------------------------------------------------------------------------
--- File Name    : https://oracle-base.com/dba/monitoring/index_usage.sql
--- Author       : Tim Hall
 -- Description  : Shows the usage for the specified table indexes.
 -- Call Syntax  : @index_usage (table-name) (index-name or all)
 -- Last Modified: 04/02/2005
