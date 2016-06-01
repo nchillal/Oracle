e8d903fa7027ffe1bd175056d9e7e9fcccd848ba
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-05-31 23:18:53 -0700

    Update cr_tracefile_loc.sql

--- cr_tracefile_loc.sql
+++ cr_tracefile_loc.sql
@@ -11,21 +11,24 @@ set lines 80
 set pages 22   
 set head off  
 
-SELECT 'Request id: '||request_id ,  
-'Trace id: '||oracle_Process_id,  
-'Trace Flag: '||req.enable_trace,  
-'Trace Name:  
-'||dest.value||'/'||lower(dbnm.value)||'_ora_'||oracle_process_id||'.trc',  
-'Prog. Name: '||prog.user_concurrent_program_name,  
-'File Name: '||execname.execution_file_name|| execname.subroutine_name ,  
-'Status : '||decode(phase_code,'R','Running')  
-||'-'||decode(status_code,'R','Normal'),  
-'SID Serial: '||ses.sid||','|| ses.serial#,  
-'Module : '||ses.module  
-from apps.fnd_concurrent_requests req, v$session ses, v$process proc,  
-v$parameter dest, v$parameter dbnm, apps.fnd_concurrent_programs_vl prog,  
-apps.fnd_executables execname  
-where req.request_id = &request  
+SELECT  'Request id: '||request_id ,  
+        'Trace id: '||oracle_Process_id,  
+        'Trace Flag: '||req.enable_trace,  
+        'Trace Name: '||dest.value||'/'||LOWER(dbnm.value)||'_ora_'||oracle_process_id||'.trc',  
+        'Prog. Name: '||prog.user_concurrent_program_name,  
+        'File Name: '||execname.execution_file_name|| execname.subroutine_name ,  
+        'Status : '||DECODE(phase_code,'R','Running')  
+        ||'-'||DECODE(status_code,'R','Normal'),  
+        'SID Serial: '||ses.sid||','|| ses.serial#,  
+        'Module : '||ses.module  
+from    apps.fnd_concurrent_requests req, 
+        v$session ses, 
+        v$process proc,  
+        v$parameter dest, 
+        v$parameter dbnm, 
+        apps.fnd_concurrent_programs_vl prog,  
+        apps.fnd_executables execname  
+where   req.request_id = &request  
 and req.oracle_process_id=proc.spid(+)  
 and proc.addr = ses.paddr(+)  
 and dest.name='user_dump_dest'  
