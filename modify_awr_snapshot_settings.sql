71cec6e71b586f81396595be827428fd1822956e
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-06-01 10:48:51 -0700

    Update modify_awr_snapshot_settings.sql

--- modify_awr_snapshot_settings.sql
+++ modify_awr_snapshot_settings.sql
@@ -1,8 +1,10 @@
 # Purpose: Modify AWR Snapshot Settings. 
 
 BEGIN
-  DBMS_WORKLOAD_REPOSITORY.modify_snapshot_settings(
+  DBMS_WORKLOAD_REPOSITORY.modify_snapshot_settings
+    (
     retention => &retention_mins,  
-    interval  => &interval_mins); 
+    interval  => &interval_mins
+    ); 
 END;
 /
