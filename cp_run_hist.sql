7e2ba4ba2c479bc78c96418d966d91de464ddae5
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-07-20 14:03:37 -0700

    Update cp_run_hist.sql

--- cp_run_hist.sql
+++ cp_run_hist.sql
@@ -1,10 +1,10 @@
 set linesize 220
-col progName format a39
-col logfile_name format a55 word_wrap
-col argument_text format a30
-col "Total Run Time" for a30
+COLUMN progName FORMAT a39
+COLUMN logfile_name FORMAT a55 word_wrap
+COLUMN argument_text FORMAT a30
+COLUMN "Total Run Time" FORMAT a30
 
-select    request_id,
+SELECT    request_id,
           p.user_concurrent_program_name progName,
         	argument_text,
         	r.logfile_name,
@@ -13,8 +13,8 @@ select    request_id,
         	EXTRACT(MINUTE FROM (actual_completion_date - actual_start_date) DAY TO SECOND ) || ' minutes' "Total Run Time"
 FROM    	applsys.fnd_concurrent_requests r, apps.fnd_concurrent_programs_tl p
 WHERE   	p.concurrent_program_id = r.concurrent_program_id
-and     	r.phase_code='C'
-and     	r.status_code='C'
-and     	p.user_concurrent_program_name = '&ucpn'
+AND     	r.phase_code='C'
+AND     	r.status_code='C'
+AND     	p.user_concurrent_program_name = '&ucpn'
 ORDER BY 	1
 /
