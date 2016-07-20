818077bda961316402521b8d8e98e6ec059ade28
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-07-20 14:07:34 -0700

    Update mysql-dbsize.sql

--- mysql-dbsize.sql
+++ mysql-dbsize.sql
@@ -1,5 +1,5 @@
 SELECT      table_schema "Data Base Name",
-            SUM( data_length + index_length ) / 1024 / 1024 "Data Base Size in MB",
-            SUM( data_free )/ 1024 / 1024 "Free Space in MB"
+            SUM(data_length + index_length) / 1024 / 1024 "Data Base Size in MB",
+            SUM(data_free)/ 1024 / 1024 "Free Space in MB"
 FROM        information_schema.TABLES
 GROUP BY    table_schema;
