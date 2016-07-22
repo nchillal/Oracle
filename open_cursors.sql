f5f0f6ec5733b39fef21643361218f46170b46e1
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-07-22 15:20:29 -0700

    Update open_cursors.sql

--- open_cursors.sql
+++ open_cursors.sql
@@ -1,13 +1,23 @@
-set pages230 lines110 trims on 
-clear col 
-col name format a30  
-col username format a30 
-break on username nodup skip 1 
+SET pages 230 lines 110 trims on 
+COLUMN name FORMAT a30  
+COLUMN username FORMAT a30 
+BREAK ON username NODUP SKIP 1 
 
-select vses.username||':'||vsst.sid||','||vses.serial# username, vstt.name, max(vsst.value) value  
-from v$sesstat vsst, v$statname vstt, v$session vses 
-where vstt.statistic# = vsst.statistic# and vsst.sid = vses.sid and vstt.name in  
-('session pga memory','session pga memory max','session uga memory','session uga memory max',  
-'session cursor cache count','session cursor cache hits','session stored procedure space', 
-'opened cursors current','opened cursors cumulative') and vses.username is not null 
-group by vses.username, vsst.sid, vses.serial#, vstt.name order by vses.username, vsst.sid, vses.serial#, vstt.name; 
+SELECT    vses.username||':'||vsst.sid||','||vses.serial# username, vstt.name, MAX(vsst.value) value  
+FROM      v$sesstat vsst, v$statname vstt, v$session vses 
+WHERE     vstt.statistic# = vsst.statistic# 
+AND       vsst.sid = vses.sid 
+AND       vstt.name IN (
+                        'session pga memory',
+                        'session pga memory max',
+                        'session uga memory',
+                        'session uga memory max',  
+                        'session cursor cache count',
+                        'session cursor cache hits',
+                        'session stored procedure space', 
+                        'opened cursors current',
+                        'opened cursors cumulative'
+                        ) 
+AND       vses.username IS NOT NULL 
+GROUP BY  vses.username, vsst.sid, vses.serial#, vstt.name 
+ORDER BY  vses.username, vsst.sid, vses.serial#, vstt.name; 
