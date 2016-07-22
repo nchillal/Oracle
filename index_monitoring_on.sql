4fcec71ff550c1fbe24184aff96ee5df103e509a
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-07-22 15:16:03 -0700

    Update index_monitoring_on.sql

--- index_monitoring_on.sql
+++ index_monitoring_on.sql
@@ -8,10 +8,10 @@ SET FEEDBACK OFF
 SET VERIFY OFF
 SPOOL temp.sql
 
-SELECT 'ALTER INDEX "' || i.owner || '"."' || i.index_name || '" MONITORING USAGE;'
-FROM   dba_indexes i
-WHERE  owner      = UPPER('&1')
-AND    table_name = DECODE(UPPER('&2'), 'ALL', table_name, UPPER('&2'));
+SELECT  'ALTER INDEX "' || i.owner || '"."' || i.index_name || '" MONITORING USAGE;'
+FROM    dba_indexes i
+WHERE   owner = UPPER('&1')
+AND     table_name = DECODE(UPPER('&2'), 'ALL', table_name, UPPER('&2'));
 
 SPOOL OFF
 
