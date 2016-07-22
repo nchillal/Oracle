a511de1c59d999922847854d00644e2f2f8ed0cb
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-07-21 18:15:28 -0700

    Update longops.sql

--- longops.sql
+++ longops.sql
@@ -1,4 +1,12 @@
-select  * 
-from    v$session_longops 
-where   sofar<>totalwork 
-and     time_remaining!=0;
+SET linesize 200
+SET colsep |
+
+SELECT  ROUND(sofar*8/1024/1024,2) "Completed[GB]",
+        ROUND(totalwork*8/1024/1024,2) "TotalWork[GB]", 
+        ROUND((sofar*100)/totalwork,2) "%Completed", 
+        TO_CHAR(TRUNC(elapsed_seconds/3600),'FM9900') || ':' || TO_CHAR(TRUNC(MOD(elapsed_seconds,3600)/60),'FM00') || ':' || TO_CHAR(MOD(elapsed_seconds,60),'FM00') "Time Completed", TO_CHAR(TRUNC(time_remaining/3600),'FM9900') || ':' || TO_CHAR(TRUNC(MOD(time_remaining,3600)/60),'FM00') || ':' || TO_CHAR(MOD(time_remaining,60),'FM00') "Time Remaining"
+FROM    v$session_longops 
+WHERE   UPPER(opname) like '%RMAN%' 
+AND     sofar <> totalwork 
+AND     totalwork <> 0 
+AND     opname='RMAN: aggregate input';
