02bc1e29193dbbb3c46d85270bf85e1dd8da22e5
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-02 21:39:32 -0700

    Update objtyp.sql

--- objtyp.sql
+++ objtyp.sql
@@ -1,10 +1,10 @@
-set lines 230
-col object_name for a30
+SET lines 230
+COLUMN object_name FOR a30
 
-select  owner, 
+SELECT  owner, 
         object_name, 
         object_type, 
         last_ddl_time, 
         status 
-from    dba_objects 
-where   object_name = UPPER('&objtect_name');
+FROM    dba_objects 
+WHERE   object_name = UPPER('&objtect_name');
