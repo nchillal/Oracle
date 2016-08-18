778a9cc8ab2b4deded6f84b01bdcf3275a41527d
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-17 23:09:54 -0700

    Update dbms_explain.sql

--- dbms_explain.sql
+++ dbms_explain.sql
@@ -1,5 +1,5 @@
-set lines 230 pages 200
-col PLAN_TABLE_OUTPUT for a140
+SET lines 230 pages 200
+COLUMN plan_table_output FORMAT a140
 
 SELECT  * 
 FROM    table(DBMS_XPLAN.DISPLAY_CURSOR('&sql_id',&child_number))
