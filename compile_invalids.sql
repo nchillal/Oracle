24be64851e4db237a43d5faa256e0c162174d5d0
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-17 23:09:10 -0700

    Update compile_invalids.sql

--- compile_invalids.sql
+++ compile_invalids.sql
@@ -1,23 +1,23 @@
 set pagesize 0 head off feedb off echo off
 spool validate_all.sql
 
-select 'alter '||object_type||' '||owner||'.'||object_name||' compile;'
-from dba_objects where object_type in ('PROCEDURE','FUNCTION','VIEW','TRIGGER','MATERIALIZED VIEW')
+SELECT 'alter '||object_type||' '||owner||'.'||object_name||' compile;'
+FROM dba_objects WHERE object_type IN ('PROCEDURE','FUNCTION','VIEW','TRIGGER','MATERIALIZED VIEW')
 and status='INVALID' order by owner
 /
-select 'alter package '||owner||'.'||object_name||' compile package;'
-from dba_objects where object_type in ('PACKAGE')
+SELECT 'alter package '||owner||'.'||object_name||' compile package;'
+FROM dba_objects WHERE object_type IN ('PACKAGE')
 and status='INVALID' order by owner
 /
-select 'alter package '||owner||'.'||object_name||' compile body;'
-from dba_objects where object_type in ('PACKAGE BODY')
+SELECT 'alter package '||owner||'.'||object_name||' compile body;'
+FROM dba_objects WHERE object_type IN ('PACKAGE BODY')
 and status='INVALID' order by owner
 /
-select 'ALTER JAVA SOURCE "' || object_name || '" COMPILE;'
-from user_objects where object_type = 'JAVA SOURCE' and status = 'INVALID';
+SELECT 'ALTER JAVA SOURCE "' || object_name || '" COMPILE;'
+FROM user_objects WHERE object_type = 'JAVA SOURCE' and status = 'INVALID';
 /
-select 'ALTER JAVA CLASS "' || object_name || '" RESOLVE;'
-from user_objects where object_type = 'JAVA CLASS' and status = 'INVALID';
+SELECT 'ALTER JAVA CLASS "' || object_name || '" RESOLVE;'
+FROM user_objects WHERE object_type = 'JAVA CLASS' and status = 'INVALID';
 /
 spool off
 exit
