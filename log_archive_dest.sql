c5cfcb884c73cd64f3cedf640d03f010f364ec5d
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-22 12:05:23 -0700

    Update log_archive_dest.sql

--- log_archive_dest.sql
+++ log_archive_dest.sql
@@ -2,11 +2,12 @@ SET lines 155 pages 200
 COLUMN name FORMAT a40
 COLUMN value FORMAT a80
 
-SELECT  name, value
-FROM    v$spparameter
-WHERE   REGEXP_LIKE(name, 'log_archive_dest_+\d')
+SELECT  name, value 
+FROM    v$spparameter 
+WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND regexp_like (value, 'service=*')
 UNION 
 SELECT  name, value
 FROM    v$spparameter
 WHERE   REGEXP_LIKE(name, 'log_archive_dest_state_+\d')
+AND     REGEXP_LIKE(value, '
 /
