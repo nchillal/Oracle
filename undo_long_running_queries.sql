97bf1441106449248e13dcbe8964310b0ae02ff3
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-23 10:06:42 -0700

    Update undo_long_running_queries.sql

--- undo_long_running_queries.sql
+++ undo_long_running_queries.sql
@@ -1,4 +1,5 @@
-COLUMN sql_fulltext FORMAT a90
+COLUMN sql_fulltext FORMAT a100
+COLUMN "Last Load" FORMAT a25
 
 SELECT  sql_id, sql_fulltext, last_load_time "Last Load", ROUND(elapsed_time/60/60/24) "Elapsed Days" 
 FROM    v$sql 
