d7da4dec06558fd12d87d4c6660fe5485aecb4ed
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-23 09:28:53 -0700

    Create undo_tbsp_usage.sql

---
+++ undo_tbsp_usage.sql
@@ -0,0 +1,15 @@
+SELECT  a.tablespace_name, SIZEMB, USAGEMB, (SIZEMB - USAGEMB) FREEMB
+FROM      (
+          SELECT SUM(bytes) / 1024 / 1024 SIZEMB, b.tablespace_name
+          FROM      dba_data_files a, dba_tablespaces b
+          WHERE     a.tablespace_name = b.tablespace_name
+          AND       b.contents = 'UNDO'
+          GROUP BY  b.tablespace_name
+          ) a,
+          (
+          SELECT    c.tablespace_name, SUM(bytes) / 1024 / 1024 USAGEMB
+          FROM      dba_undo_extents c
+          WHERE     status <> 'EXPIRED'
+          GROUP BY  c.tablespace_name
+          ) b
+WHERE     a.tablespace_name = b.tablespace_name;
