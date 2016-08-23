59cafa48e0beb1d3c83e0bc3602f3208d9f6e71b
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-23 10:04:39 -0700

    Update undo_long_running_queries.sql

--- undo_long_running_queries.sql
+++ undo_long_running_queries.sql
@@ -1,3 +1,5 @@
+COLUMN sql_fulltext FORMAT a90
+
 SELECT  sql_id, sql_fulltext, last_load_time "Last Load", ROUND(elapsed_time/60/60/24) "Elapsed Days" 
 FROM    v$sql 
 WHERE   sql_id IN (SELECT maxquerysqlid FROM sys.wrh$_undostat WHERE status > 1)
