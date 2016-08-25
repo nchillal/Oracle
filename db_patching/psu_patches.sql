4fb5fb92f26e97d2d87c3a78fdaa598a93a87572
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-25 00:23:50 -0700

    Capitalized keywords

--- db_patching/psu_patches.sql
+++ db_patching/psu_patches.sql
@@ -1,14 +1,14 @@
 set lines 230 pages 50
 
-col action_time for a30 
-col comments for a50
-col bundle for a6
+COLUMN action_time FORMAT a30
+COLUMN comments FORMAT a50
+COLUMN bundle FORMAT a6
 
-select  SUBSTR(action_time,1,30) 
+SELECT  SUBSTR(action_time,1,30)
         action_time,
         SUBSTR(id,1,10) id,
         SUBSTR(action,1,10) action,
         SUBSTR(version,1,8) version,
         SUBSTR(BUNDLE_SERIES,1,6) bundle,
         SUBSTR(comments,1,20) comments
-from    registry$history; 
+FROM    registry$history;
