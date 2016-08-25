afa392e075e9025838e52783f001fa76c05e4b84
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-25 00:17:05 -0700

    Added columns to v$database

--- db_status.sql
+++ db_status.sql
@@ -1,7 +1,10 @@
-set lines 200
+set lines 200 numwidth 20
 COLUMN host_name FORMAT a40
+COLUMN open_mode FORMAT a10
 
 ALTER SESSION SET NLS_DATE_FORMAT='DD_MON-YYYY HH24:MI:SS';
+
+show parameter service
 
 SELECT    instance_name,
           startup_time,
@@ -14,5 +17,12 @@ FROM      gv$instance;
 
 SELECT    name,
           open_mode,
-          database_role
+          database_role,
+          log_mode,
+          checkpoint_change#,
+          controlfile_type,
+          protection_mode,
+          protection_level,
+          switchover_status,
+          dataguard_broker "DGMGRL"
 FROM      v$database;
