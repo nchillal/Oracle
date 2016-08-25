1b18b6f5933657ddc3fecc5817b66f16470d1ad8
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-25 00:07:16 -0700

    Added few more columns for in-depth information.

--- db_status.sql
+++ db_status.sql
@@ -1,8 +1,15 @@
 set lines 200
+COLUMN host_name FORMAT a40
+
+ALTER SESSION SET NLS_DATE_FORMAT='DD_MON-YYYY HH24:MI:SS';
+
 SELECT    instance_name,
+          startup_time,
           host_name,
           status,
-          active_state 
+          active_state,
+          database_status,
+          logins
 FROM      gv$instance;
 
 SELECT    name,
