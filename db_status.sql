cd9c97accef92ec09aac547a06cced99e3437c1f
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-25 00:00:18 -0700

    moved files to folder

--- db_status.sql
+++ db_status.sql
@@ -1,10 +1,11 @@
-set lines 200 
-SELECT    instance_name, 
-          host_name, 
-          status 
+set lines 200
+SELECT    instance_name,
+          host_name,
+          status,
+          active_state 
 FROM      gv$instance;
 
-SELECT    name, 
-          open_mode, 
-          database_role 
-FROM      v$database; 
+SELECT    name,
+          open_mode,
+          database_role
+FROM      v$database;
