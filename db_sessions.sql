e1fa40535dff7d444511dd576199b80d43f6fd86
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-08-29 14:06:26 -0700

    Changed

--- db_sessions.sql
+++ db_sessions.sql
@@ -1,4 +1,11 @@
-SELECT    username, status, sql_id, count(*) 
-FROM      v$session 
-WHERE     username LIKE '%_USER' 
+SELECT    username, status, sql_id, count(*)
+FROM      v$session
+WHERE     username LIKE '%_USER'
 GROUP BY  username, status, sql_id;
+
+BREAK ON username
+
+SELECT    username, status, COUNT(*)
+FROM      v$session
+GROUP BY  username, status
+ORDER BY  1,3 desc;
