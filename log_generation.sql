a9349ee0c0a0f6a9b517d65cba3e703c06d7a968
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-09-05 11:44:33 -0700

    Update log_generation.sql

--- log_generation.sql
+++ log_generation.sql
@@ -1,13 +1,36 @@
-SELECT    TO_CHAR(first_time, 'DD-MON-YYYY'), COUNT(*)
-FROM      v$log_history
-WHERE     TO_CHAR(first_time, 'DD-MON-YYYY') >= to_char(SYSDATE - &num_days, 'DD-MON-YYYY')
-GROUP BY  TO_CHAR(first_time, 'DD-MON-YYYY')
-ORDER BY  1
-/
-
-select    TO_CHAR(first_time, 'HH24'), COUNT(*)
-from      v$log_history
-where     TO_CHAR(first_time, 'DD-MON-YYYY') = to_char(SYSDATE - &num_days, 'DD-MON-YYYY')
-group by  TO_CHAR(first_time, 'HH24')
-order by  1
+SELECT * 
+FROM  (
+      SELECT  first_time, TO_CHAR(first_time, 'DD-MON-YYYY') as d, TO_CHAR(first_time, 'HH24') as h
+      FROM    v$log_history      
+      )
+PIVOT (
+      COUNT(TO_CHAR(first_time, 'DD-MON-YYYY')) 
+      FOR h IN  (
+                '00' as H00,
+                '01' as H01,
+                '02' as H02,
+                '03' as H03,
+                '04' as H04,
+                '05' as H05,
+                '06' as H06,
+                '07' as H07,
+                '08' as H08,
+                '09' as H09,
+                '10' as H10,
+                '11' as H11,
+                '12' as H12,
+                '13' as H13,
+                '14' as H14,
+                '15' as H15,
+                '16' as H16,
+                '17' as H17,
+                '18' as H18,
+                '19' as H19,
+                '20' as H20,
+                '21' as H21,
+                '22' as H22,
+                '23' as H23
+                )
+)
+ORDER BY TO_DATE(d)
 /
