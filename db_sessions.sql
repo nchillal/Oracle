8a7097d2036abcabf7e147a3615bf318e3f462ba
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-09-06 12:24:16 -0700

    Changes

--- db_sessions.sql
+++ db_sessions.sql
@@ -12,6 +12,19 @@ PIVOT     (
 ORDER BY  username
 ;
 
+-- This query returns machine, username and their status count
+SELECT    *
+FROM      (
+	       SELECT  machine, username, status
+	       FROM    v$session
+	       WHERE   username LIKE '%_USER'
+	       )
+PIVOT     (
+	       COUNT(status)
+	       FOR (status) IN ('ACTIVE' as ACTIVE, 'INACTIVE' as INACTIVE)
+	       )
+ORDER BY  machine
+
 -- This query returns username, sql they are executing and their status count
 SELECT    *
 FROM      (
