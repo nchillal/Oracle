587f5fa663ae40ae4cfaf2aa7b095d11f649828e
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-09-06 20:34:20 -0700

    Changes

--- db_performance/waitevents.sql
+++ db_performance/waitevents.sql
@@ -5,15 +5,25 @@ BREAK ON inst_id
 COLUMN event FORMAT a50
 COLUMN machine FORMAT a40
 
-SELECT  inst_id, 
+SELECT  inst_id,
         sid,
         username,
-        event, 
-        seconds_in_wait, 
-        status, 
-        machine, 
-        program 
-FROM    gv$session 
-WHERE   wait_time=0 
-AND     type <> 'BACKGROUND' 
-AND     event NOT IN ('dispatcher timer','pipe get','pmon timer','PX Idle Wait','PX Deq Credit: need buffer','rdbms ipc message','shared server idle wait','smon timer','SQL*Net message from client');
+        event,
+        seconds_in_wait,
+        status,
+        machine,
+        program
+FROM    gv$session
+WHERE   wait_time=0
+AND     type <> 'BACKGROUND'
+AND     event NOT IN  (
+                      'dispatcher timer',
+                      'pipe get',
+                      'pmon timer',
+                      'PX Idle Wait',
+                      'PX Deq Credit: need buffer',
+                      'rdbms ipc message',
+                      'shared server idle wait',
+                      'smon timer',
+                      'SQL*Net message from client'
+                      );
