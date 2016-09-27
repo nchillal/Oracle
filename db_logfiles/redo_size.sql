d23ed9b9fb5a18362c451c594d372aa7fe99b3bf
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-09-27 00:23:04 -0700

    Redo generation per second

--- db_logfiles/redo_size.sql
+++ db_logfiles/redo_size.sql
@@ -4,7 +4,7 @@ WHERE     TO_CHAR(FIRST_TIME, 'DD-MON-YYYY') > TO_CHAR(SYSDATE - &num_days, 'DD-
 GROUP BY  TO_CHAR(FIRST_TIME, 'DD-MON-YYYY')
 ORDER BY  TO_DATE(TO_CHAR(FIRST_TIME, 'DD-MON-YYYY'));
 
-SELECT    TO_CHAR(FIRST_TIME, 'DD-MON-YYYY') as "Date", SUM(blocks * block_size)/1024/1024/1024 as "Total Redo (GB)"
-FROM      v$archived_log
-GROUP BY  TO_CHAR(FIRST_TIME, 'DD-MON-YYYY')
-ORDER BY  TO_DATE(TO_CHAR(FIRST_TIME, 'DD-MON-YYYY'));
+SELECT    begin_time, end_time, intsize_csec, value, metric_unit
+FROM      v$sysmetric_history
+WHERE     metric_name = 'Redo Generated Per Sec'
+ORDER BY  begin_time;
