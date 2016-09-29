5d3c15a3540e63f73f702699592e85de0b7044ae
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-09-29 11:38:50 -0700

    Changes

--- db_performance/last_analyzed.sql
+++ db_performance/last_analyzed.sql
@@ -1,3 +1,3 @@
-SELECT    sample_size, partitioned, last_analyzed, num_rows, global_stats, user_stats
+SELECT    owner, sample_size, partitioned, last_analyzed, num_rows, global_stats, user_stats
 FROM      dba_tables
 WHERE     table_name='&table_name';
