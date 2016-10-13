a986a78a1e615b817eaa94765ff43c7e7b3ccbd5
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-10-12 22:56:49 -0700

    Changes

---
+++ object_details.sql
@@ -0,0 +1,24 @@
+SET linesize 230 echo off pagesize 200
+COLUMN object_name FOR a30
+COLUMN segment_name FOR a30
+ACCEPT ONAME PROMPT 'Enter Object Name:'
+
+SELECT    owner,
+          object_id,
+          object_name,
+          object_type,
+          last_ddl_time,
+          status
+FROM      dba_objects
+WHERE     object_name = UPPER('&ONAME');
+
+SELECT    segment_name, segment_type, SUM(bytes/1024/1024) "Size (MB)"
+FROM      dba_segments
+WHERE     segment_name='&ONAME'
+GROUP BY  segment_name, segment_type
+/
+
+SELECT    owner, table_name, sample_size, partitioned, last_analyzed, num_rows, global_stats, user_stats
+FROM      dba_tables
+WHERE     table_name='&ONAME'
+/
---
+++ standby_check.sql
@@ -0,0 +1,33 @@
+-- This query display dataguard process status. 
+SELECT    inst_id, pid, delay_mins, process, status, group#, thread#, sequence#, block#, blocks
+FROM      gv$managed_standby
+/
+
+-- This query checks if MRP is running in Real-Time apply mode.
+SELECT    recovery_mode
+FROM      v$archive_dest_status
+WHERE     recovery_mode <> 'IDLE'
+/
+
+-- Verify the last sequence# received and the last sequence# applied to standby database.
+SELECT    al.thrd "Thread", almax "Last Seq Received", lhmax "Last Seq Applied"
+FROM      (
+          SELECT    thread# thrd, MAX(sequence#) almax
+          FROM      v$archived_log
+          WHERE     resetlogs_change# = (SELECT resetlogs_change# FROM v$database)
+          GROUP BY  thread#
+          ) al,
+          (
+          SELECT    thread# thrd,
+                    MAX(sequence#) lhmax
+          FROM      v$log_history
+          WHERE     resetlogs_change#=(SELECT resetlogs_change# FROM v$database)
+          GROUP BY  thread#
+          ) lh
+WHERE     al.thrd = lh.thrd
+/
+
+SELECT    value, time_computed
+FROM      v$dataguard_stats
+WHERE     name='apply lag'
+/
