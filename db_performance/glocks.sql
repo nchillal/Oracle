7e95968c1c025c2b0c07ce6b80772cd08978133a
Nagendra Chillale <nchillal@apple.com>
2016-12-04 16:24:36 -0800

    Added query from v$session to detect locking.

--- db_performance/glocks.sql
+++ db_performance/glocks.sql
@@ -1,5 +1,10 @@
-SELECT    INST_ID, 
+SELECT    INST_ID,
           DECODE(request,0,'Holder: ','Waiter: ') || sid sess, id1, id2, lmode, request, type
 FROM      GV$LOCK
 WHERE     (id1, id2, type) IN (SELECT id1, id2, type FROM GV$LOCK WHERE request > 0)
 ORDER BY  id1, request;
+
+SELECT    blocking_session, sid, serial#, sql_id, p1text, p1, p2text, p2, p3text, p3, wait_class, seconds_in_wait
+FROM      v$session
+WHERE     blocking_session IS NOT NULL
+ORDER BY  blocking_session;
