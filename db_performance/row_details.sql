4b657d3bf7c6d25e8c6442606b00a40bce11d2d0
Nagendra Chillale <nchillal@apple.com>
2016-12-07 07:59:12 -0800

    Changes

--- db_performance/row_details.sql
+++ db_performance/row_details.sql
@@ -8,4 +8,5 @@ AND     state='WAITING'
 -- Detail of row based on object, file, block and row number.
 SELECT  ownerid
 FROM    &table_name
-WHERE   rowid = dbms_rowid.rowid_create(&rowid_type, &row_wait_obj, &row_wait_file, &row_wait_block, &row_wait_row);
+WHERE   rowid = dbms_rowid.rowid_create(&rowid_type, &row_wait_obj, &row_wait_file, &row_wait_block, &row_wait_row)
+;
