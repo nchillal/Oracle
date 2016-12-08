d351932b47f1238de1cf1c447ca6eac552ac6441
Nagendra Chillale <nchillal@apple.com>
2016-12-07 18:08:50 -0800

    New file for wait_events_by_sqlid.sql

---
+++ db_performance/wait_events_by_sqlid.sql
@@ -0,0 +1,14 @@
+SELECT    event,
+          time_waited "time_waited(s)",
+          CASE WHEN time_waited = 0 THEN
+            0
+          ELSE
+            ROUND(time_waited*100 / SUM(time_waited) OVER(), 2)
+          END "percentage"
+FROM      (
+          SELECT    event, SUM(time_waited) time_waited
+          FROM      v$active_session_history
+          WHERE     sql_id = '&sql_id'
+          GROUP BY  event
+          )
+ORDER BY  time_waited DESC;
