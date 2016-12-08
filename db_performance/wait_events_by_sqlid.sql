e2c24025ec35e351102a1385a77b5ef857e83686
Nagendra Chillale <nchillal@apple.com>
2016-12-07 18:12:30 -0800

    Added to obtain from AWR.

--- db_performance/wait_events_by_sqlid.sql
+++ db_performance/wait_events_by_sqlid.sql
@@ -1,3 +1,4 @@
+-- To get it from Shared Pool.
 SELECT    event,
           time_waited "time_waited(s)",
           CASE WHEN time_waited = 0 THEN
@@ -10,5 +11,22 @@ FROM      (
           FROM      v$active_session_history
           WHERE     sql_id = '&sql_id'
           GROUP BY  event
+          )
+ORDER BY  time_waited DESC;
+
+
+-- To obtain from AWR.
+SELECT    event,
+          time_waited "time_waited(s)",
+          CASE WHEN time_waited = 0 THEN
+            0
+          ELSE
+            round(time_waited*100 / sum(time_waited) Over(), 2)
+          END "percentage"
+from      (
+          SELECT event, sum(time_waited) time_waited
+          FROM dba_hist_active_sess_history
+          WHERE sql_id = '&sql_id'
+          GROUP BY event
           )
 ORDER BY  time_waited DESC;
