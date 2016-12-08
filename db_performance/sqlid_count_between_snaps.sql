13a683a4047ed6b3f4759234df6775bb13ee70d8
Nagendra Chillale <nchillal@apple.com>
2016-12-07 23:35:24 -0800

    New file sqlid count between snapshots.

---
+++ db_performance/sqlid_count_between_snaps.sql
@@ -0,0 +1,32 @@
+declare
+	min_snapid number(10);
+	max_snapid number(10);
+begin
+	select min(snap_id), max(snap_id) into min_snapid, max_snapid from dba_hist_active_sess_history where SAMPLE_TIME > SYSDATE - &min/1440;
+	IF min_snapid = max_snapid THEN
+		min_snapid := min_snapid - 1;
+	END IF;
+
+	dbms_output.put_line(chr(10)||'Min snapid: '||min_snapid);
+	dbms_output.put_line('Max snapid: '||max_snapid);
+	dbms_output.put_line(chr(10)||'Top SQL_ID being executed between snapshots '||min_snapid||' and '||max_snapid);
+	dbms_output.put_line(chr(10)||'--------------------------------------------------------');
+	dbms_output.put_line(RPAD('SQL_ID', 15, ' ')||'        '||RPAD('Child Number', 15, ' ')||'        '||'Count');
+	dbms_output.put_line('--------------------------------------------------------');
+  FOR row IN  (
+              SELECT    *
+              FROM      (
+                        SELECT    sql_id, sql_child_number, COUNT(*) count
+                        FROM      dba_hist_active_sess_history
+                        WHERE     snap_id between min_snapid AND max_snapid
+                        AND       sql_id IS NOT NULL
+                        GROUP BY  sql_id, sql_child_number
+                        ORDER BY  3 DESC
+                        )
+              WHERE     rownum < 10
+              )
+  LOOP
+    dbms_output.put_line(RPAD(row.sql_id, 15, ' ')||'        '||RPAD(row.sql_child_number, 15, ' ')||'        '||row.count);
+  END LOOP;
+end;
+/
