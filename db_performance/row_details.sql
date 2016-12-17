de6a3a1c662542fa56a39872676d83b133428221
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-12-17 12:28:36 -0800

    Changes

--- db_performance/row_details.sql
+++ db_performance/row_details.sql
@@ -1,9 +1,9 @@
 -- Details of the object / block / row that is causing wait.
-SELECT  row_wait_obj#, row_wait_file#, row_wait_block#, row_wait_row#
-FROM    v$session
-WHERE   event='&event_name'
-AND     state='WAITING'
-;
+  SELECT  row_wait_obj#, row_wait_file#, row_wait_block#, row_wait_row#
+  FROM    v$session
+  WHERE   event='&event_name'
+  AND     state='WAITING'
+  ;
 
 -- Detail of row based on object, file, block and row number.
 SELECT  ownerid
