a5f3956d747259516a736ec142d908e11f2ccf9b
Nagendra Chillale <nagendra.chillale@yahoo.com>
2016-12-21 16:56:29 -0800

    Changes

--- tcps_check.sql
+++ tcps_check.sql
@@ -1,9 +1,9 @@
 set serveroutput on
 BEGIN
-  IF SYS_CONTEXT('USERENV','NETWORK_PROTOCOL')!='TCPS' THEN
-    DBMS_OUTPUT.PUT_LINE(chr(10)||'TCPS is enabled.');
-  ELSE
+  IF SYS_CONTEXT('USERENV','NETWORK_PROTOCOL')!='tcps' THEN
     DBMS_OUTPUT.PUT_LINE(chr(10)||'TCPS is not enabled.');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE(chr(10)||'TCPS is enabled.');
   END IF;
 END;
 /
