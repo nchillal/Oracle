c42051c22f43422de4bcf3960fade4ef82993506
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-01-01 15:38:39 -0800

    Changes

---
+++ asm_disk_mount_status.sql
@@ -0,0 +1,8 @@
+set linesize 1000 pagesize 100
+COLUMN path FORMAT a40
+COLUMN name FORMAT a15
+COLUMN failgroup FORMAT a15
+
+SELECT    group_number, disk_number, failgroup,name "Disk Name", mount_status, path
+FROM      v$asm_disk
+ORDER BY  failgroup, name, path;
