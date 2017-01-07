dc1fc060f45583bd291e102b49b3e99baaa6f79d
Nagendra Chillale <nchillal@apple.com>
2017-01-06 22:32:48 -0800

    Changes

--- db_performance/profile_advisory.sql
+++ db_performance/profile_advisory.sql
@@ -19,11 +19,11 @@ BEGIN
               benefit
       FROM    (
               SELECT    CASE
-                          WHEN attr5 LIKE 'OPT_ESTIMATE%' THEN cast(attr5 as varchar2(4000))
+                          WHEN attr5 LIKE 'OPT_ESTIMATE%' THEN cast(attr5 as VARCHAR2(4000))
                           WHEN attr1 LIKE 'OPT_ESTIMATE%' THEN attr1
                         END hint,
                         benefit
-              FROM      dba_advisor_recommendations t join dba_advisor_rationale r using (task_id,rec_id)
+              FROM      dba_advisor_recommendations t JOIN dba_advisor_rationale r USING (task_id,rec_id)
               WHERE     t.owner=o.owner
               AND       t.task_name = o.task_name
               AND       r.object_id=o.object_id
@@ -32,7 +32,7 @@ BEGIN
               )
               ORDER BY  TO_NUMBER(REGEXP_REPLACE(hint,'^.*=([0-9.]+)[^0-9].*$','1'))
               ) loop
-    DBMS_OUTPUT.PUT_LINE('   '||r.hint); benefit:=to_number(r.benefit)/100;
+    DBMS_OUTPUT.PUT_LINE('   '||r.hint); benefit:=TO_NUMBER(r.benefit)/100;
     END LOOP;
     DBMS_OUTPUT.PUT_LINE('*/');
     -- get the outline hints
@@ -40,7 +40,7 @@ BEGIN
       SELECT  outline_data INTO outline_data
       FROM    (
               SELECT    CASE
-                          WHEN other_xml IS NOT NULL THEN EXTRACT(xmltype(other_xml),'/*/outline_data/hint')
+                          WHEN other_xml IS NOT NULL THEN EXTRACT(XMLTYPE(other_xml),'/*/outline_data/hint')
                         END outline_data
               FROM      dba_advisor_tasks t JOIN dba_sqltune_plans p USING (task_id)
               WHERE     t.owner=o.owner
@@ -62,7 +62,7 @@ BEGIN
       DBMS_OUTPUT.PUT_LINE('   '||r.hint);
     END LOOP;
     DBMS_OUTPUT.PUT_LINE('*/');
-    DBMS_OUTPUT.PUT_LINE('--- Benefit: '||to_char(to_number(benefit),'FM99.99')||'%');
+    DBMS_OUTPUT.PUT_LINE('--- Benefit: '||TO_CHAR(TO_NUMBER(benefit),'FM99.99')||'%');
   END LOOP;
   DBMS_OUTPUT.PUT_LINE('');
 END;
