202b8d81a24cf963cb3fddda76dab70779eeea29
Nagendra Chillale <nchillal@apple.com>
2017-01-07 17:44:01 -0800

    Changes

--- db_performance/purge_plan_sqlid.sql
+++ db_performance/purge_plan_sqlid.sql
@@ -1,5 +1,12 @@
-SELECT 	address, hash_value, plan_hash_value
-FROM 	  v$sqlarea
-WHERE 	sql_id='&sql_id';
+DECLARE
+  v_address VARCHAR2(30);
+  v_hash_value VARCHAR2(30);
+  v_plan_hash_value VARCHAR2(30);
+BEGIN
+  SELECT 	address, hash_value, plan_hash_value INTO v_address, v_hash_value, v_plan_hash_value
+  FROM 	  v$sqlarea
+  WHERE 	sql_id='&sql_id';
 
-exec DBMS_SHARED_POOL.PURGE('&address, &hash_value', 'C');
+  EXEC DBMS_SHARED_POOL.PURGE(''''||v_address||','||v_hash_value||'''', 'C');
+END;
+/
