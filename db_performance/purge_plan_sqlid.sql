787ce4fd9d40514c8312fec1931c0f965c745dfe
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-01-12 14:43:23 -0800

    Changes

--- db_performance/purge_plan_sqlid.sql
+++ db_performance/purge_plan_sqlid.sql
@@ -1,12 +1,5 @@
-DECLARE
-  v_address VARCHAR2(30);
-  v_hash_value VARCHAR2(30);
-  v_plan_hash_value VARCHAR2(30);
-BEGIN
-  SELECT 	address, hash_value, plan_hash_value INTO v_address, v_hash_value, v_plan_hash_value
-  FROM 	  v$sqlarea
-  WHERE 	sql_id='&sql_id';
+SELECT 	address, hash_value, plan_hash_value
+FROM 	  v$sqlarea
+WHERE 	sql_id='&sql_id';
 
-  EXEC DBMS_SHARED_POOL.PURGE(''''||v_address||','||v_hash_value||'''', 'C');
-END;
-/
+EXEC DBMS_SHARED_POOL.PURGE('&address,&hash_value', 'C');
