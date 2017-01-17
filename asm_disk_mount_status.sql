01ddd54510837468dff19d5c3884cf534b665a08
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-01-16 18:55:40 -0800

    Changes

--- asm_disk_mount_status.sql
+++ asm_disk_mount_status.sql
@@ -1,6 +1,7 @@
 set linesize 1000 pagesize 100
 COLUMN path FORMAT a40
 COLUMN name FORMAT a15
+COLUMN "Disk Name" FORMAT a20
 COLUMN failgroup FORMAT a15
 
 SELECT    group_number, disk_number, failgroup,name "Disk Name", mount_status, path
