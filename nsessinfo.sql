52062eff8274da3217134b1cf7fb7b32a2098379
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-01-30 19:24:36 -0800

    Changes

--- nsessinfo.sql
+++ nsessinfo.sql
@@ -1,8 +1,8 @@
-rem ********************************************************************
-rem * Filename          : sessinfo.sql - Version 1.0
-rem * Description       : Information about a specified session
-rem * Usage             : start sessinfo.sql
-rem ********************************************************************
+-- ********************************************************************
+-- Filename          : sessinfo.sql - Version 1.0
+-- Description       : Information about a specified session
+-- Usage             : start sessinfo.sql
+-- ********************************************************************
 
 def aps_prog    = 'sessinfo.sql'
 def aps_title   = 'Session information'
@@ -17,13 +17,13 @@ ACCEPT spid     PROMPT 'Please enter the value for Server Process if known : '
 ACCEPT osuser   PROMPT 'Please enter the value for OS User if known        : '
 ACCEPT username PROMPT 'Please enter the value for DB User if known        : '
 SELECT  ' Sid, Serial#, Aud sid : '|| s.sid||' , '||s.serial#||' , '||
-       s.audsid||' , '||s.inst_id||chr(10)|| '     DB User / OS User : '||s.username||
-       '   /   '||s.osuser||chr(10)|| '    Machine - Terminal : '||
-       s.machine||'  -  '|| s.terminal||chr(10)||
+       s.audsid||' , '||s.inst_id||CHR(10)|| '     DB User / OS User : '||s.username||
+       '   /   '||s.osuser||CHR(10)|| '    Machine - Terminal : '||
+       s.machine||'  -  '|| s.terminal||CHR(10)||
        '        OS Process Ids : '||
-       s.process||' (Client)  '||p.spid||' (Server)'|| ' (Since) '||to_char(s.logon_time,'DD-MON-YYYY HH24:MI:SS')||chr(10)||
-       '   Client Program Name : '||s.program||chr(10) "Session Info",
-	   '   Action / Module     : '||s.action||'  / '||s.module||chr(10) ||'User Name : '
+       s.process||' (Client)  '||p.spid||' (Server)'|| ' (Since) '||TO_CHAR(s.logon_time,'DD-MON-YYYY HH24:MI:SS')||CHR(10)||
+       '   Client Program Name : '||s.program||CHR(10) "Session Info",
+	   '   Action / Module     : '||s.action||'  / '||s.module||CHR(10) ||'User Name : '
 FROM    gv$process p,gv$session s
 WHERE   p.addr = s.paddr
 AND     s.sid = NVL('&SID',s.sid)
