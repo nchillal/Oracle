cf168a9091cd2e78be7087a95e2e008ac1f08a59
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-02-08 22:10:31 -0800

    Update ad_bugs.sql

--- ebs_patching/ad_bugs.sql
+++ ebs_patching/ad_bugs.sql
@@ -1,5 +1,5 @@
-set linesize 230
-COLUMN APPLICATION_SHORT_NAME FORMAT a6 HEADING ASN
+SET LINESIZE 230
+COLUMN application_short_name FORMAT a6 HEADING ASN
 SELECT  *
 FROM    apps.ad_bugs
 WHERE   bug_number='&bug_number';
