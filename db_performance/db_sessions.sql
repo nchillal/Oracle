683f7d8bd3a6ab367f5ed222f8fa6360b8a64d36
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-02-17 14:06:58 -0800

    Changes

--- db_performance/db_sessions.sql
+++ db_performance/db_sessions.sql
@@ -4,7 +4,7 @@ SELECT    *
 FROM      (
           SELECT  inst_id, username, status
           FROM    gv$session
-          WHERE   username LIKE '%_USER'
+          WHERE   type <> 'BACKGROUND'
           )
 PIVOT     (
           COUNT(status)
@@ -14,57 +14,46 @@ ORDER BY  inst_id, username
 ;
 
 -- This query returns machine, username and their status count
+BREAK ON inst_id SKIP 1
 SELECT    *
 FROM     (
-	       SELECT  machine, username, status
-	       FROM    v$session
-	       WHERE   username LIKE '%_USER'
+	       SELECT  inst_id, machine, username, status
+	       FROM    gv$session
+	       WHERE   type <> 'BACKGROUND'
 	       )
 PIVOT    (
 	       COUNT(status)
 	       FOR (status) IN ('ACTIVE' as ACTIVE, 'INACTIVE' as INACTIVE)
 	       )
-ORDER BY  machine
+ORDER BY  inst_id, machine
 ;
 
 -- This query returns username, logon_time and their count
+BREAK ON inst_id SKIP 1
 SELECT    *
 FROM      (
-          SELECT  username, status, TO_CHAR(LOGON_TIME, 'DD-MON-YYYY HH24:MI') LOGON_TIME
+          SELECT  inst_id, username, status, TO_CHAR(LOGON_TIME, 'DD-MON-YYYY HH24:MI') LOGON_TIME
           FROM    v$session
-          WHERE   username LIKE '%_USER'
+          WHERE   type <> 'BACKGROUND'
           )
 PIVOT     (
           COUNT(status)
           FOR (status) IN ('ACTIVE' as ACTIVE, 'INACTIVE' as INACTIVE)
           )
-ORDER BY  username
-;
-
--- This query returns username, event, status and their count
-SELECT    *
-FROM      (
-          SELECT  username, event, status
-          FROM    v$session
-          WHERE   username LIKE '%_USER'
-          )
-PIVOT     (
-          COUNT(status)
-          FOR (status) IN ('ACTIVE' as ACTIVE, 'INACTIVE' as INACTIVE)
-          )
-ORDER BY  username
+ORDER BY  inst_id, username
 ;
 
 -- This query returns username, sql they are executing and their status count
+BREAK ON inst_id SKIP 1
 SELECT    *
 FROM      (
-          SELECT  username, event, sql_id, status
-          FROM    v$session
-          WHERE   username like '%_USER'
+          SELECT  inst_id, username, event, sql_id, status
+          FROM    gv$session
+          WHERE   type <> 'BACKGROUND'
           )
 PIVOT     (
           COUNT(status)
           FOR (status) IN ('ACTIVE' as ACTIVE, 'INACTIVE' as INACTIVE)
           )
-ORDER BY  username, sql_id, event
+ORDER BY  inst_id, username, sql_id, event
 ;
