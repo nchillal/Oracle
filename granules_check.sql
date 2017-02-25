61cbe9fb0d4e433ecd3b83cf1463cbfe5c5c6cfd
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-02-25 13:35:48 -0800

    Update granules_check.sql

--- granules_check.sql
+++ granules_check.sql
@@ -1,3 +1,3 @@
-SELECT  inst_id,'granules_count='||(current_size - user_specified_size) / granule_size granules_avail
+SELECT  inst_id, 'Granules_Count='||(current_size - user_specified_size) / granule_size granules_avail
 FROM    gv$sga_dynamic_components
 WHERE   component LIKE 'DEFAULT buffer cache';
