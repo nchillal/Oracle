6a679307cd7203cedf85cd186ff74373a6e18e2a
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-03-02 14:54:22 -0800

    New file for generating create sequence statements

---
+++ db_sequence/gen_create_sequence.sql
@@ -0,0 +1,3 @@
+SELECT  'CREATE SEQUENCES '||sequence_owner||'.'||sequence_name||' START WITH '||last_number||' INCREMENT BY '||increment_by||';'
+FROM    dba_sequences
+WHERE   sequence_name LIKE 'MME_SST%'
