37622a28ff742664aeb22de73db610b6aa08db57
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-03-14 07:50:14 -0700

    Changes

--- db_logfiles/redo_size.sql
+++ db_logfiles/redo_size.sql
@@ -11,21 +11,13 @@ SET PAGESIZE 200 LINESIZE 155
 BREAK ON DAY SKIP 1
 COMPUTE SUM LABEL 'TOTAL' -
         AVG LABEL 'AVERAGE' -
-        OF "REDO (MB)" ON DAY
-SELECT    TO_CHAR(BEGIN_TIME, 'DD-MON-YYYY') "Day", TO_CHAR(BEGIN_TIME, 'HH24') "HR", SUM(value) "REDO (MB)"
-FROM      (
-          SELECT    begin_time, value/1024/1024 value
-          FROM      dba_hist_sysmetric_history
-          WHERE     metric_name = 'Redo Generated Per Sec'
-          UNION
-          SELECT    begin_time, value/1024/1024 "REDO (MB)"
-          FROM      v$sysmetric_history
-          WHERE     metric_name = 'Redo Generated Per Sec'
-          ORDER BY  begin_time
-          )
-WHERE     begin_time > SYSDATE - &hrs/24
-GROUP BY  TO_CHAR(begin_time, 'DD-MON-YYYY'), TO_CHAR(BEGIN_TIME, 'HH24')
-ORDER BY  TO_DATE(TO_CHAR(begin_time, 'DD-MON-YYYY')), TO_CHAR(BEGIN_TIME, 'HH24');
+        OF "Total Redo (GB)" ON DAY
+SELECT    TO_CHAR(FIRST_TIME, 'DD-MON-YYYY') as "Day", TO_CHAR(FIRST_TIME, 'HH24') as "Hour", SUM(blocks * block_size)/1024/1024/1024 as "Total Redo (GB)"
+FROM      v$archived_log
+WHERE     DEST_ID = 1
+AND       TO_CHAR(FIRST_TIME, 'DD-MON-YYYY') > TO_CHAR(SYSDATE - &num_days, 'DD-MON-YYYY')
+GROUP BY  TO_CHAR(FIRST_TIME, 'DD-MON-YYYY'), TO_CHAR(FIRST_TIME, 'HH24')
+ORDER BY  TO_DATE(TO_CHAR(FIRST_TIME, 'DD-MON-YYYY'));
 
 -- Query to obtain redo generation per min.
 SELECT    *
