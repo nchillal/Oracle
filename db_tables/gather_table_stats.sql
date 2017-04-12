33a7a9f9bf38f06e578185378e856e45f50a43b7
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-04-11 23:06:50 -0700

    Changes

--- db_tables/gather_table_stats.sql
+++ db_tables/gather_table_stats.sql
@@ -1,1 +1,1 @@
-EXEC DBMS_STATS.GATHER_TABLE_STATS('&owner', '&table_name', estimate_percent => dbms_stats.auto_sample_size);
+EXEC DBMS_STATS.GATHER_TABLE_STATS('&owner', '&table_name', estimate_percent => dbms_stats.auto_sample_size, cascade => TRUE, method_opt => 'FOR ALL COLUMNS SIZE AUTO');
