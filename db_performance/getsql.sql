6de9e63448f5f6d44bb259aab2dd2d965e79ffa7
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-04-18 11:21:37 -0700

    Added dbms_lob for sql_fulltext

--- db_performance/getsql.sql
+++ db_performance/getsql.sql
@@ -2,7 +2,7 @@ BREAK ON hash_value SKIP 1
 SET PAGESIZE 1000 LINESIZE 160
 COLUMN sql_fulltext FORMAT a140
 
-SELECT 		inst_id, sql_fulltext
+SELECT 		inst_id, DBMS_LOB.SUBSTR(sql_fulltext, 5000, 1) sql_fulltext
 FROM 			gv$sqlarea
 WHERE			sql_id = '&sql_id'
 /
@@ -14,6 +14,6 @@ WHERE 		hash_value = (SELECT sql_hash_value FROM v$session WHERE sid = &sid)
 ORDER BY 	piece
 /
 
-SELECT 		sql_id, sql_child_number 
+SELECT 		sql_id, sql_child_number
 FROM 			v$session
 WHERE			sid=&sid;
