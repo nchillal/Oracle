c6178667fb7c46bccd500a87d93be4b43fa32af9
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-04-19 20:54:53 -0700

    Index build monitoring script

---
+++ db_indexes/monitor_index_build.sql
@@ -0,0 +1,17 @@
+COLUMN "Index Operation" FORMAT a60 trunc
+COLUMN "ETA Mins" FORMAT 9999.99
+COLUMN "Runtime Mins" FORMAT 9999.99
+select    sess.sid AS "Session ID",
+          sql.sql_text AS "Index Operation",
+          longops.totalwork, longops.sofar,
+          longops.elapsed_seconds/60 AS "Runtime Mins",
+          longops.time_remaining/60 AS "ETA Mins"
+FROM      v$session sess, v$sql sql, v$session_longops longops
+WHERE     sess.sid=longops.sid
+AND       sess.sql_address = sql.address
+AND       sess.sql_address = longops.sql_address
+AND       sess.status  = 'ACTIVE'
+AND       longops.totalwork > longops.sofar
+AND       sess.sid NOT IN ( SELECT SYS_CONTEXT('USERENV', 'SID') sid FROM dual)
+AND       UPPER(sql.sql_text) LIKE '%INDEX%'
+ORDER BY  3,4;
