9a28178e17a5a27196b16341e7a77cd6f4337f92
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-04-19 20:21:28 -0700

    Changes

--- ASM/asm_usage_by_filetype.sql
+++ ASM/asm_usage_by_filetype.sql
@@ -4,8 +4,8 @@ COLUMN Size_MB FORMAT 99,999,999.00
 SELECT    a.type,
           SUM(a.bytes)/1048576 size_mb,
           b.total_mb asm_volume_size_mb,
-          ((SUM(a.bytes)/1048576)/b.TOTAL_MB)*100 AS "Percent_Used"
+          ((SUM(a.bytes)/1048576)/b.total_mb)*100 AS "Percent_Used"
 FROM      v$asm_file a,v$asm_diskgroup b
 WHERE     a.group_number=b.group_number
-GROUP BY  a.type,b.TOTAL_MB
+GROUP BY  a.type,b.total_mb
 ORDER BY  4;
