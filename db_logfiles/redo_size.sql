b787b1e5e7cf0361918ef841f09d19b5ae3f0d32
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-04-22 11:06:43 -0700

    CHanges

--- db_logfiles/redo_size.sql
+++ db_logfiles/redo_size.sql
@@ -11,13 +11,24 @@ SET PAGESIZE 1000 LINESIZE 155
 BREAK ON DAY SKIP 1
 COMPUTE SUM LABEL 'TOTAL' AVG LABEL 'AVERAGE' OF "Total Redo (GB)" ON DAY
 SELECT    TO_CHAR(FIRST_TIME, 'DD-MON-YYYY') as "Day", TO_CHAR(FIRST_TIME, 'HH24') as "Hour", SUM(blocks * block_size)/1024/1024/1024 as "Total Redo (GB)"
-FROM      v$archived_log
+FROM      gv$archived_log
 WHERE     DEST_ID = 1
 AND       FIRST_TIME > SYSDATE - interval '&hours' hour
 GROUP BY  TO_CHAR(FIRST_TIME, 'DD-MON-YYYY'), TO_CHAR(FIRST_TIME, 'HH24')
 ORDER BY  TO_DATE(TO_CHAR(FIRST_TIME, 'DD-MON-YYYY')), TO_CHAR(FIRST_TIME, 'HH24');
 
--- Query to obtain redo generation per min.
+-- Query to obtain redo generation per hour.
+SET PAGESIZE 1000 LINESIZE 155
+BREAK ON DAY SKIP 1
+COMPUTE SUM LABEL 'TOTAL' AVG LABEL 'AVERAGE' OF "Total Redo (GB)" ON DAY
+SELECT    TO_CHAR(FIRST_TIME, 'DD-MON-YYYY') as "Day", TO_CHAR(FIRST_TIME, 'HH24:MI') as "Hour", SUM(blocks * block_size)/1024/1024/1024 as "Total Redo (GB)"
+FROM      gv$archived_log
+WHERE     DEST_ID = 1
+AND       FIRST_TIME > SYSDATE - interval '&hours' hour
+GROUP BY  TO_CHAR(FIRST_TIME, 'DD-MON-YYYY'), TO_CHAR(FIRST_TIME, 'HH24:MI')
+ORDER BY  TO_DATE(TO_CHAR(FIRST_TIME, 'DD-MON-YYYY')), TO_CHAR(FIRST_TIME, 'HH24:MI');
+
+-- Query to obtain redo generation per sec.
 SELECT    *
 FROM      (
           SELECT    begin_time, end_time, value/1024/1024 "REDO (MB)"
