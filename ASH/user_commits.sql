8c6e3b33cfac0c3727e5df05e84715863696c3a1
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-04-22 18:56:46 -0700

    Changes

---
+++ ASH/user_commits.sql
@@ -0,0 +1,17 @@
+BREAK ON snap_id SKIP 1
+SELECT    *
+FROM      (
+          SELECT  snap_id,
+                  instance_number,
+                  end_time,
+                  metric_name,
+                  maxval
+          FROM    dba_hist_sysmetric_summary
+          WHERE   end_time >= SYSDATE - INTERVAL '&days' day
+          AND     REGEXP_LIKE(instance_number, '&instance_number')
+          )
+PIVOT     (
+          MAX(maxval)
+          FOR metric_name IN ('User Commits Per Sec' AS USER_COMMITS_PER_SEC)
+          )
+ORDER BY  snap_id;
