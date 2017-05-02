bdda3b41269ac427a4ec53f7ae2bac86d74879b0
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-05-02 10:04:59 -0700

    Changes

--- ASH/sqlid_count_ash.sql
+++ ASH/sqlid_count_ash.sql
@@ -1,15 +1,23 @@
-SELECT    sql_id, sql_child_number, COUNT(*)
-FROM      v$active_session_history
-WHERE     sample_time > TO_DATE('&start_datetime','MMDDYYHH24MI')
-AND       sample_time < TO_DATE('&end_datetime','MMDDYYHH24MI')
-AND       user_id > 0
-GROUP BY  sql_id, sql_child_number
-ORDER BY  COUNT(*);
+SELECT  *
+FROM    (
+        SELECT    sql_id, sql_child_number, COUNT(*)
+        FROM      v$active_session_history
+        WHERE     sample_time > TO_DATE('&start_datetime','MMDDYYHH24MI')
+        AND       sample_time < TO_DATE('&end_datetime','MMDDYYHH24MI')
+        AND       user_id > 0
+        GROUP BY  sql_id, sql_child_number
+        ORDER BY  COUNT(*) DESC
+        )
+WHERE   rownum <= 10;
 
-SELECT    sql_id,sql_child_number, COUNT(*)
-FROM      DBA_HIST_ACTIVE_SESS_HISTORY
-WHERE     sample_time > TO_DATE('&start_datetime','MMDDYYHH24MI')
-AND       sample_time < TO_DATE('&end_datetime','MMDDYYHH24MI')
-AND       user_id > 0
-GROUP BY  sql_id, sql_child_number
-ORDER BY  COUNT(*);
+SELECT  *
+FROM    (
+        SELECT    sql_id,sql_child_number, COUNT(*)
+        FROM      DBA_HIST_ACTIVE_SESS_HISTORY
+        WHERE     sample_time > TO_DATE('&start_datetime','MMDDYYHH24MI')
+        AND       sample_time < TO_DATE('&end_datetime','MMDDYYHH24MI')
+        AND       user_id > 0
+        GROUP BY  sql_id, sql_child_number
+        ORDER BY  COUNT(*)
+        )
+WHERE   rownum <= 10;
