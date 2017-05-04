4a36cdf4307c4fb3e1993f597eb741f5946cef87
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-05-03 22:37:20 -0700

    Changes

--- db_tables/get_lobs_for_table.sql
+++ db_tables/get_lobs_for_table.sql
@@ -1,9 +1,13 @@
 SET LINESIZE 155 PAGESIZE 2000
 COLUMN column_name FORMAT a30
+COLUMN tablespace_name FORMAT a20
+COLUMN owner FORMAT a20
+COLUMN index_name FORMAT a25
 SELECT    s.owner,
           s.tablespace_name,
           l.segment_name,
           l.column_name,
+          l.index_name,
           s.header_file,
           s.header_block
 FROM      dba_segments s, dba_lobs l
@@ -11,4 +15,4 @@ WHERE     s.owner=l.owner
 AND       s.segment_name=l.segment_name
 AND       s.segment_type='LOBSEGMENT'
 AND       s.owner='&owner_name'
-AND       l.table_name='&table_name'; 
+AND       l.table_name='&table_name';
