a9602fa7e4ef733d53e16cbca40fca05bde2323b
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-05-10 08:25:05 -0700

    Changes

--- db_performance/getsql.sql
+++ db_performance/getsql.sql
@@ -1,19 +1,23 @@
-BREAK ON hash_value SKIP 1
 SET PAGESIZE 1000 LINESIZE 160
-COLUMN sql_fulltext FORMAT a140
 
+BREAK ON username
+COLUMN "sid, serial#" FORMAT a15
+SELECT 		s.sid||','||s.serial# "sid, serial#", s.status, s.username, s.sql_id, s.sql_child_number, s.sql_hash_value, p.plan_hash_value
+FROM 			v$session s, v$sql_plan p
+WHERE			s.sql_id = p.sql_id
+AND 			s.sql_child_number = p.child_number
+AND 			s.sql_id='&sql_id';
+
+BREAK ON username
+COLUMN "sid, serial#" FORMAT a15
+SELECT 		s.sid||','||s.serial# "sid, serial#", s.username, s.event, p.sql_id, p.child_number, s.sql_hash_value, p.plan_hash_value
+FROM 			v$session s, v$sql_plan p
+WHERE			s.sql_id = p.sql_id
+AND 			s.sql_child_number = p.child_number
+AND 			status='ACTIVE';
+
+COLUMN sql_fulltext FORMAT a140
 SELECT 		inst_id, DBMS_LOB.SUBSTR(sql_fulltext, 5000, 1) sql_fulltext
 FROM 			gv$sqlarea
 WHERE			sql_id = '&sql_id'
 /
-
-SELECT		hash_value,
-					DBMS_LOB.SUBSTR(sql_text, 5000, 1) sql_text
-FROM 			v$sqltext
-WHERE 		hash_value = (SELECT sql_hash_value FROM v$session WHERE sid = &sid)
-ORDER BY 	piece
-/
-
-SELECT 		sql_id, sql_child_number
-FROM 			v$session
-WHERE			sid=&sid;
