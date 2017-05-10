0cfcb71e7d2f7cf66a2080835768d2498ae587b6
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-05-10 08:25:50 -0700

    Changes

--- db_performance/dbms_explain.sql
+++ db_performance/dbms_explain.sql
@@ -1,6 +1,6 @@
 SET linesize 230 pagesize 200
 COLUMN plan_table_output FORMAT a140
 
-SELECT  * 
-FROM    table(DBMS_XPLAN.DISPLAY_CURSOR('&sql_id',&child_number))
+SELECT  *
+FROM    table(DBMS_XPLAN.DISPLAY_CURSOR('&sql_id', &child_number))
 /
