8cd3cb9faa110653a580e8502ca9ab3889678eec
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-05-10 19:56:48 -0700

    Update sqlid_baseline.sql

--- db_performance/sqlid_baseline.sql
+++ db_performance/sqlid_baseline.sql
@@ -2,5 +2,5 @@ BREAK ON sql_id SKIP 1
 
 SELECT  sql_id, child_number, sql_handle, plan_name, a.parsing_schema_name
 FROM    dba_sql_plan_baselines a, v$sql b
-WHERE   a.signature = b.EXACT_MATCHING_SIGNATURE
-AND     a.signature = b.FORCE_MATCHING_SIGNATURE;
+WHERE   a.signature = b.exact_matching_signature
+AND     a.signature = b.force_matching_signature;
