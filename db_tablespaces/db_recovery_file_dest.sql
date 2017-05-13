0007e7d72eead31c263feef1929de25aa141f76d
Nagendra Chillale <nchillal@apple.com>
2017-05-13 13:10:30 -0700

    Changes

--- db_tablespaces/db_recovery_file_dest.sql
+++ db_tablespaces/db_recovery_file_dest.sql
@@ -5,10 +5,10 @@ COLUMN used_mb FORMAT 999,999,999
 COLUMN pct_used FORMAT 999
 
 SELECT    name,
-          CEIL( space_limit / 1024 / 1024) SIZE_M,
-          CEIL( space_used  / 1024 / 1024) USED_M,
-          CEIL( space_reclaimable  / 1024 / 1024) RECLAIMABLE_M,
-          DECODE(NVL(space_used, 0), 0, 0, CEIL(((space_used - space_reclaimable) / space_limit) * 100)) PCT_USED
+          CEIL(space_limit/1024/1024) SIZE_M,
+          CEIL(space_used/1024/1024) USED_M,
+          CEIL(space_reclaimable/1024/1024) RECLAIMABLE_M,
+          DECODE(NVL(space_used, 0), 0, 0, CEIL(((space_used - space_reclaimable)/space_limit)*100)) PCT_USED
 FROM      v$recovery_file_dest
 ORDER BY  name
 /
