d038473e682988ad3b7200d38edf1a9845ba9bde
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-05-24 13:36:57 -0700

    Changes

--- roleddl.sql
+++ roleddl.sql
@@ -3,8 +3,8 @@
 -- Call Syntax  : @role_ddl (role)
 -- -----------------------------------------------------------------------------------
 
-set long 20000 longchunksize 20000 pagesize 0 linesize 1000 feedback off verify off trimspool on
-column ddl format a1000
+SET LONG 20000 LONGCHUNKSIZE 20000 PAGESIZE 0 LINESIZE 1000 FEEDBACK OFF VERIFY OFF TRIMSPOOL ON
+COLUMN ddl FORMAT a1000
 
 begin
    dbms_metadata.set_transform_param (dbms_metadata.session_transform, 'SQLTERMINATOR', true);
