cf1bf70ac0f7b42078d6b51948019545d5ed989a
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-05-31 13:29:36 -0700

    Added object space usage.

--- object_unused_space.sql
+++ object_unused_space.sql
@@ -10,7 +10,7 @@ DECLARE
 BEGIN
   dbms_space.unused_space('&schema','&object','&type', TOTAL_BLOCKS, TOTAL_BYTES, UNUSED_BLOCKS, UNUSED_BYTES, LAST_USED_EXTENT_FILE_ID, LAST_USED_EXTENT_BLOCK_ID, LAST_USED_BLOCK);
   dbms_output.put_line('-----------------------------------');
-  dbms_output.put_line('UNUSED SPACE                       ');
+  dbms_output.put_line('           UNUSED SPACE            ');
   dbms_output.put_line('-----------------------------------');
   dbms_output.put_line('TOTAL_BLOCKS              = '||TOTAL_BLOCKS);
   dbms_output.put_line('TOTAL_BYTES               = '||TOTAL_BYTES);
@@ -19,5 +19,16 @@ BEGIN
   dbms_output.put_line('LAST_USED_EXTENT_FILE_ID  = '||LAST_USED_EXTENT_FILE_ID);
   dbms_output.put_line('LAST_USED_EXTENT_BLOCK_ID = '||LAST_USED_EXTENT_BLOCK_ID);
   dbms_output.put_line('LAST_USED_BLOCK           = '||LAST_USED_BLOCK);
+END;
+/
+
+SET SERVEROUTPUT ON
+DECLARE
+  su NUMBER;
+  sa NUMBER;
+BEGIN
+  dbms_space.object_space_usage('&segment_owner', '&segment_name', '&segment_type', NULL, su, sa);
+  dbms_output.put_line('Space Used: '|| ROUND(TO_CHAR(su)/1024, 2)||' K');
+  dbms_output.put_line('Space Allocated: ' || ROUND(TO_CHAR(sa)/1024, 2)||' K');
 END;
 /
