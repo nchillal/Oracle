2c67b202a531510ffc23ee08164e9d090d06143d
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-05-31 13:30:45 -0700

    Changes

--- object_unused_space.sql
+++ object_unused_space.sql
@@ -1,24 +1,24 @@
 SET SERVEROUTPUT ON
 DECLARE
-  TOTAL_BLOCKS              number;
-  TOTAL_BYTES               number;
-  UNUSED_BLOCKS             number;
-  UNUSED_BYTES              number;
-  LAST_USED_EXTENT_FILE_ID  number;
-  LAST_USED_EXTENT_BLOCK_ID number;
-  LAST_USED_BLOCK           number;
+  total_blocks              number;
+  total_bytes               number;
+  unused_blocks             number;
+  unused_bytes              number;
+  last_used_extent_file_id  number;
+  last_used_extent_block_id number;
+  last_used_block           number;
 BEGIN
-  dbms_space.unused_space('&schema','&object','&type', TOTAL_BLOCKS, TOTAL_BYTES, UNUSED_BLOCKS, UNUSED_BYTES, LAST_USED_EXTENT_FILE_ID, LAST_USED_EXTENT_BLOCK_ID, LAST_USED_BLOCK);
+  dbms_space.unused_space('&schema','&object','&type', total_blocks, total_bytes, unused_blocks, unused_bytes, last_used_extent_file_id, last_used_extent_block_id, last_used_block);
   dbms_output.put_line('-----------------------------------');
   dbms_output.put_line('           UNUSED SPACE            ');
   dbms_output.put_line('-----------------------------------');
-  dbms_output.put_line('TOTAL_BLOCKS              = '||TOTAL_BLOCKS);
-  dbms_output.put_line('TOTAL_BYTES               = '||TOTAL_BYTES);
-  dbms_output.put_line('UNUSED_BLOCKS             = '||UNUSED_BLOCKS);
-  dbms_output.put_line('UNUSED BYTES              = '||UNUSED_BYTES);
-  dbms_output.put_line('LAST_USED_EXTENT_FILE_ID  = '||LAST_USED_EXTENT_FILE_ID);
-  dbms_output.put_line('LAST_USED_EXTENT_BLOCK_ID = '||LAST_USED_EXTENT_BLOCK_ID);
-  dbms_output.put_line('LAST_USED_BLOCK           = '||LAST_USED_BLOCK);
+  dbms_output.put_line('TOTAL_BLOCKS              = '||total_blocks);
+  dbms_output.put_line('TOTAL_BYTES               = '||total_bytes);
+  dbms_output.put_line('UNUSED_BLOCKS             = '||unused_blocks);
+  dbms_output.put_line('UNUSED BYTES              = '||unused_bytes);
+  dbms_output.put_line('LAST_USED_EXTENT_FILE_ID  = '||last_used_extent_file_id);
+  dbms_output.put_line('LAST_USED_EXTENT_BLOCK_ID = '||last_used_extent_block_id);
+  dbms_output.put_line('LAST_USED_BLOCK           = '||last_used_block);
 END;
 /
 
