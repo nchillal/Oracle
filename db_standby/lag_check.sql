6a2f645a64b930956cc8382bac436383d5dbb9a5
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-06-21 20:18:51 -0700

    Added order by clause

--- db_standby/lag_check.sql
+++ db_standby/lag_check.sql
@@ -1,5 +1,6 @@
 SET LINESIZE 230
 BREAK ON inst_id SKIP 1
-SELECT  inst_id, name, value, time_computed
-FROM		gv$dataguard_stats
-WHERE 	name IN ('transport lag', 'apply lag');
+SELECT    inst_id, name, value, time_computed
+FROM      gv$dataguard_stats
+WHERE     name IN ('transport lag', 'apply lag')
+ORDER BY  inst_id;
