35094f51ab95c14d0f69daccaddc8a3461bfc4ae
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-06-29 14:47:37 -0700

    Changes

--- db_performance/getsql.sql
+++ db_performance/getsql.sql
@@ -4,10 +4,10 @@ BREAK ON username
 COLUMN username FORMAT a20
 COLUMN event FORMAT a45
 COLUMN "sid, serial#" FORMAT a15
-SELECT 		s.sid||','||s.serial# "sid, serial#", s.username, module, s.event, p.sql_id, p.child_number, s.sql_hash_value, p.plan_hash_value
-FROM 			v$session s, v$sql_plan p
-WHERE			s.sql_id = p.sql_id
-AND 			s.sql_child_number = p.child_number
+SELECT 		s.sid||','||s.serial# "sid, serial#", s.username, s.module, s.event, p.sql_id, s.sql_child_number, s.sql_hash_value, p.plan_hash_value
+FROM 			v$session s, v$sqlarea p
+WHERE			s.sql_hash_value = p.hash_value
+AND       s.sql_address = p.address
 AND 			status='ACTIVE';
 
 BREAK ON username
