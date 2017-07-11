f0d6d4b89dc871dacbb659d19a32ff3753169e0c
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-07-10 20:32:40 -0700

    Changes

--- ASH/session_count_ash.sql
+++ ASH/session_count_ash.sql
@@ -1,5 +1,5 @@
 SET LINESIZE 155 PAGESIZE 2000
-BREAK on username SKIP 1
+BREAK ON username SKIP 1
 SELECT    du.username username, TO_CHAR(sample_time,'MON-DD-YYYY') DAY, TO_CHAR(sample_time,'HH24:MI') HH24MM, COUNT(*)
 FROM      v$active_session_history ash, dba_users du
 WHERE     ash.user_id = du.user_id
@@ -10,7 +10,7 @@ GROUP BY  du.username, TO_CHAR(sample_time,'MON-DD-YYYY'), TO_CHAR(sample_time,'
 ORDER BY  1,2,3;
 
 SET LINESIZE 155 PAGESIZE 2000
-BREAK on username SKIP 1
+BREAK ON username SKIP 1
 SELECT    du.username username, TO_CHAR(sample_time,'MON-DD-YYYY') DAY, TO_CHAR(sample_time,'HH24:MI') HH24MM, COUNT(*)
 FROM      dba_hist_active_sess_history dash, dba_users du
 WHERE     dash.user_id = du.user_id
