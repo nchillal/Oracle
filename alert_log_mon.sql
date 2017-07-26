90fc420baf439014555c6dda01ed44647cc14323
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-07-25 23:11:32 -0700

    Changes

--- alert_log_mon.sql
+++ alert_log_mon.sql
@@ -1,9 +1,9 @@
 SET LINESIZE 200
 COLUMN originating_timestamp FORMAT a40
-COLUMN message_text FORMAT a160
+COLUMN message_text FORMAT a90
 
-SELECT    originating_timestamp,
+SELECT    TO_CHAR(originating_timestamp, 'DD-MON-YYYY HH24:MI:SS'),
           message_text
 FROM      x$dbgalertext
-WHERE     originating_timestamp > SYSDATE-1
+WHERE     originating_timestamp > SYSDATE-1/24
 ORDER BY  1;
