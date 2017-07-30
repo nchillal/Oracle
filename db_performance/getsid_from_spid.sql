8841ef3cccfd29b95c33cbc13e4144d808bc4a8b
Nagendra Chillale <nchillal@apple.com>
2017-07-30 14:05:20 -0700

    Changes

--- db_performance/getsid_from_spid.sql
+++ db_performance/getsid_from_spid.sql
@@ -1,4 +1,14 @@
-SELECT  sid, serial#, sql_id, event, type, module, client_info, status
-FROM    v$session
-WHERE   paddr IN (SELECT addr FROM v$process WHERE spid=&spid)
-/
+SET PAGESIZE 1000 LINESIZE 260
+
+COLUMN "SID, SERIAL#" FORMAT a15
+COLUMN module FORMAT a50
+COLUMN spid FORMAT a7
+COLUMN username FORMAT a20
+COLUMN event FORMAT a50
+
+SELECT  sid||','||serial# "SID, SERIAL#", b.spid, username, status, logon_time, last_call_et, sql_id, sql_child_number, event, module
+FROM    (SELECT sid, serial#, paddr, username, status, sql_id, sql_child_number, event, module, client_info, type, logon_time, last_call_et FROM v$session) a,
+        (SELECT spid, addr FROM v$process) b
+WHERE   a.paddr = b.addr
+AND     a.status = 'ACTIVE'
+AND     a.type <> 'BACKGROUND';
