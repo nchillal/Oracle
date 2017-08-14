6e759dd59cdd4621678087f6b714d89fa190196b
Nagendra Chillale <nchillal@apple.com>
2017-08-13 21:48:12 -0700

    Changes

--- ASH/object_count_ash.sql
+++ ASH/object_count_ash.sql
@@ -1,15 +1,21 @@
-SELECT    current_obj#, COUNT(*)
-FROM      v$active_session_history
+COLUMN object_name FORMAT a30
+
+SELECT    object_name, object_type, COUNT(*)
+FROM      v$active_session_history ash, dba_objects do
 WHERE     sample_time > TO_DATE('&start_datetime','MMDDYYHH24MI')
 AND       sample_time < TO_DATE('&end_datetime','MMDDYYHH24MI')
 AND       user_id > 0
-GROUP BY  current_obj#
+AND       ash.current_obj# = do.object_id
+HAVING    COUNT(*) > 10
+GROUP BY  object_name, object_type
 ORDER BY  COUNT(*);
 
-SELECT    current_obj#, COUNT(*)
+SELECT    object_name, object_type, COUNT(*)
 FROM      dba_hist_active_sess_history
 WHERE     sample_time > TO_DATE('&start_datetime','MMDDYYHH24MI')
 AND       sample_time < TO_DATE('&end_datetime','MMDDYYHH24MI')
 AND       user_id > 0
-GROUP BY  current_obj#
+AND       ash.current_obj# = do.object_id
+HAVING    COUNT(*) > 10
+GROUP BY  object_name, object_type
 ORDER BY  COUNT(*);
--- rebuild_index.sql
+++ rebuild_index.sql
@@ -2,7 +2,7 @@ sqlplus -s "/as sysdba" <<!
 SET LINES 155 PAGES 2000 FEED OFF HEAD OFF ECHO OFF TERM OFF TRIMSPOOL ON
 SPOOL     index_partition.sql
 SELECT    'SET ECHO ON TIMING ON' FROM DUAL;
-SELECT    'ALTER INDEX '||index_owner||'.'||index_name||' REBUILD PARTITION '||PARTITION_NAME||' ONLINE PARALLEL 16;'
+SELECT    'ALTER INDEX '||index_owner||'.'||index_name||' REBUILD PARTITION '||partition_name||' ONLINE PARALLEL 16;'
 FROM      dba_ind_partitions
 WHERE     index_name='&index_name'
 ORDER BY  partition_name;
