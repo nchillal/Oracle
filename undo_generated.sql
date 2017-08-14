51d998aaceb45455c8ebb7bed71323d6772c489b
Nagendra Chillale <nchillal@apple.com>
2017-08-13 17:18:37 -0700

    Changes

--- undo_generated.sql
+++ undo_generated.sql
@@ -4,7 +4,7 @@ COMPUTE SUM LABEL 'TOTAL' OF UNDO_SIZE_MB ON begin_time
 SELECT  a.begin_time "BEGIN_TIME", a.instance_number "INSTANCE_NUMBER", ROUND((a.undoblks * b.value)/1024/1024, 2) "UNDO_SIZE_MB"
 FROM    (
           SELECT    instance_number, TO_CHAR(begin_time, 'DD-MON-YYYY') "BEGIN_TIME", SUM(undoblks) "UNDOBLKS"
-          FROM      DBA_HIST_UNDOSTAT
+          FROM      dba_hist_undostat
           WHERE     begin_time > SYSDATE - INTERVAL '&days' DAY
           GROUP BY  instance_number, TO_CHAR(begin_time, 'DD-MON-YYYY')
         ) a,
@@ -13,7 +13,7 @@ ORDER BY 1,2,3;
 
 CLEAR COMPUTE
 
-BREAK ON DAY ON HOUR_MIN SKIP 1
+BREAK ON day ON hour_min SKIP 1
 SELECT  a.day "DAY", a.hour_min "HOUR_MIN", a.instance_number "INSTANCE_NUMBER", ROUND((a.undoblks * b.value)/1024/1024, 2) "UNDO_SIZE_MB"
 FROM    (
           SELECT    instance_number, TO_CHAR(begin_time, 'DD-MON-YYYY') "DAY", TO_CHAR(begin_time, 'HH24:MI') "HOUR_MIN", SUM(undoblks) "UNDOBLKS"
