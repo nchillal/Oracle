60773e2e59b526547e4659ecc4d28f85d09d62a2
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-08-14 17:28:48 -0700

    Changes

--- ASH/sql_execution_time.sql
+++ ASH/sql_execution_time.sql
@@ -3,7 +3,12 @@ COLUMN start_time FORMAT a30
 COLUMN end_time FORMAT a30
 COLUMN time_taken FORMAT a30
 
-SELECT    sql_id, sql_exec_id, sql_plan_hash_value, MIN(sample_time) "START_TIME", MAX(sample_time) "END_TIME", EXTRACT(HOUR FROM (MAX(sample_time) - MIN(sample_time)))||' Hour '||EXTRACT(MINUTE FROM (MAX(sample_time) - MIN(sample_time))) ||' Minute '||ROUND(EXTRACT(SECOND FROM (MAX(sample_time) - MIN(sample_time)))) ||' Second ' "TIME_TAKEN"
+SELECT    sql_id,
+          sql_exec_id,
+          sql_plan_hash_value,
+          MIN(sample_time) "START_TIME",
+          MAX(sample_time) "END_TIME", 
+          EXTRACT(HOUR FROM (MAX(sample_time) - MIN(sample_time)))||' Hour '||EXTRACT(MINUTE FROM (MAX(sample_time) - MIN(sample_time))) ||' Minute '||ROUND(EXTRACT(SECOND FROM (MAX(sample_time) - MIN(sample_time)))) ||' Second ' "TIME_TAKEN"
 FROM      dba_hist_active_sess_history
 WHERE     sql_id = '&sql_id'
 AND       sample_time >= SYSDATE - INTERVAL '&days' day
