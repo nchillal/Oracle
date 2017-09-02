bdbf1319cb331abd9180539a6283ba2f0f68a68d
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-09-01 22:26:20 -0700

    Changes

--- db_performance/getsid_from_spid.sql
+++ db_performance/getsid_from_spid.sql
@@ -12,3 +12,9 @@ FROM    (SELECT sid, serial#, paddr, username, status, sql_id, sql_child_number,
 WHERE   a.paddr = b.addr
 AND     a.status = 'ACTIVE'
 AND     a.type <> 'BACKGROUND';
+
+SELECT  sid||','||serial# "SID, SERIAL#", b.spid, username, status, logon_time, last_call_et, sql_id, sql_child_number, event, module
+FROM    (SELECT sid, serial#, paddr, username, status, sql_id, sql_child_number, event, module, client_info, type, logon_time, last_call_et FROM v$session) a,
+        (SELECT spid, addr FROM v$process) b
+WHERE   a.paddr = b.addr
+AND     b.spid = &spid;
