df170f695e52f03b75cc2392427928b6dc4b15f9
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-10-03 17:25:05 -0700

    Changes

--- db_tables/table_row_count.sql
+++ db_tables/table_row_count.sql
@@ -1,3 +1,4 @@
-SELECT  'SELECT COUNT(*) FROM '||owner||'.'||table_name||';' "NUM_ROWS_TABLE"
-FROM    dba_tables
-WHERE   owner='&schema_owner';
+SELECT    'SELECT COUNT(*) FROM '||owner||'.'||table_name||';' "NUM_ROWS_TABLE"
+FROM      dba_tables
+WHERE     owner='&schema_owner'
+ORDER BY  table_name;
