26860f22e51edf64780d88659c3ef5dbbe89ec6e
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-10-05 23:04:18 -0700

    Changes

--- db_tables/table_grant.sql
+++ db_tables/table_grant.sql
@@ -1,2 +1,5 @@
 SET LINES 260 PAGES 2000
-SELECT 'GRANT '||privilege||' ON '||owner||'.'||table_name||' TO '|| grantee||';' "TABLE GRANTS" FROM dba_tab_privs WHERE table_name LIKE '&table_name' AND owner='&table_owner';
+SELECT  'GRANT '||privilege||' ON '||owner||'.'||table_name||' TO '|| grantee||';' "TABLE GRANTS"
+FROM    dba_tab_privs
+WHERE   table_name LIKE '&table_name'
+AND     owner='&table_owner';
