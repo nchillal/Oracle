1681a7b62a7b9fcb632d8ac4d9d51c2ca022cc15
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-10-10 20:48:42 -0700

    check consumer of buffer cache

--- granules_check.sql
+++ granules_check.sql
@@ -1,4 +1,21 @@
-SELECT    inst_id, 'Granules_Count='||(current_size - user_specified_size)/granule_size granules_avail
+SET NUMWIDTH 15 LINESIZE 165
+COLUMN component FORMAT a30
+SELECT    inst_id, component, current_size, granule_size, 'Granules_Count='||(current_size - user_specified_size)/granule_size granules_avail
 FROM      gv$sga_dynamic_components
 WHERE     component LIKE 'DEFAULT buffer cache'
 ORDER BY  inst_id;
+
+SELECT    bytes
+FROM      v$sgainfo
+WHERE     name LIKE 'Granule Size';
+
+BREAK ON REPORT
+COLUMN object_name FORMAT a30
+COMPUTE SUM OF BYTES ON REPORT
+COLUMN bytes FORMAT 999,999,999,999
+
+SELECT    object_name, COUNT(*)*8192 "BYTES"
+FROM      v$bh vbh, dba_objects do
+WHERE     vbh.objd = do.object_id
+GROUP BY  object_name
+ORDER BY  2;
