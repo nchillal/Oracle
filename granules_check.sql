965acc518952a3093f9a1948e20ea718cb0e1205
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-10-10 22:35:45 -0700

    Added query to report components using shared pool

--- granules_check.sql
+++ granules_check.sql
@@ -1,6 +1,6 @@
 SET NUMWIDTH 15 LINESIZE 165
 COLUMN component FORMAT a30
-SELECT    inst_id, component, current_size, granule_size, 'Granules_Count='||(current_size - user_specified_size)/granule_size granules_avail
+SELECT    inst_id, component, current_size, granule_size, 'Granules_Count - '||(current_size - user_specified_size)/granule_size granules_avail
 FROM      gv$sga_dynamic_components
 WHERE     component LIKE 'DEFAULT buffer cache'
 ORDER BY  inst_id;
@@ -19,3 +19,12 @@ FROM      v$bh vbh, dba_objects do
 WHERE     vbh.objd = do.object_id
 GROUP BY  object_name
 ORDER BY  2;
+
+SELECT    pool, SUM(bytes) "BYTES"
+FROM      v$sgastat
+GROUP BY  pool;
+
+SELECT    name, bytes, RANK() OVER(ORDER BY bytes DESC) "RANK"
+FROM      v$sgastat
+WHERE     pool='shared pool'
+AND       rownum < 20;
