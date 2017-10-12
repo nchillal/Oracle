c5b5030f9c0615b7f1fe7f0618aef6d1cf00e496
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-10-12 12:51:18 -0700

    Changes

--- db_sequence/validate_sequence.sql
+++ db_sequence/validate_sequence.sql
@@ -1,3 +1,6 @@
-SELECT  'SELECT '||sequence_owner||'.'||sequence_name||'.nextval FROM dual;'
+SELECT  'SELECT '||sequence_owner||'.'||sequence_name||'.currval FROM dual;'
 FROM    dba_sequences
 WHERE   sequence_owner='&schema';
+
+SELECT  'SELECT '||sequence_name||'.nextval FROM dual;'
+FROM    user_sequences;
