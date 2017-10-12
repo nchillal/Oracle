2a68eb73d535016c7397b5644a3314338f2c6500
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-10-11 18:30:58 -0700

    Changes

--- db_sequence/gen_create_sequence.sql
+++ db_sequence/gen_create_sequence.sql
@@ -1,3 +1,7 @@
 SELECT  'CREATE SEQUENCES '||sequence_owner||'.'||sequence_name||' START WITH '||last_number||' INCREMENT BY '||increment_by||';'
 FROM    dba_sequences
-WHERE   sequence_name LIKE 'MME_SST%'
+WHERE   sequence_owner = '&owner';
+
+SELECT  'CREATE SEQUENCES '||sequence_owner||'.'||sequence_name||' START WITH '||last_number||' INCREMENT BY '||increment_by||';'
+FROM    dba_sequences
+WHERE   sequence_name = '&name';
