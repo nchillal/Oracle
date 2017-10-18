a2eb1f024067155404051f7ca5b6e6c56a1392ae
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-10-18 13:02:56 -0700

    Changes

--- db_performance/spm_sqlid.sql
+++ db_performance/spm_sqlid.sql
@@ -2,7 +2,7 @@ SET SERVEROUTPUT ON
 DECLARE
   l_plans_loaded  PLS_INTEGER;
 BEGIN
-  l_plans_loaded := DBMS_SPM.load_plans_from_cursor_cache(sql_id => '&sql_id');
+  l_plans_loaded := DBMS_SPM.load_plans_from_cursor_cache(sql_id => '&sql_id', plan_hash_value => &plan_hash_value);
   DBMS_OUTPUT.put_line('Plans Loaded: ' || l_plans_loaded);
 END;
 /
--- db_performance/sqlid_baseline.sql
+++ db_performance/sqlid_baseline.sql
@@ -4,3 +4,29 @@ SELECT  sql_id, child_number, sql_handle, plan_name, a.parsing_schema_name
 FROM    dba_sql_plan_baselines a, v$sql b
 WHERE   a.signature = b.exact_matching_signature
 AND     a.signature = b.force_matching_signature;
+
+
+
+DECLARE
+  v_sqlid VARCHAR2(13);
+  v_num NUMBER;
+BEGIN
+  dbms_output.put_line('SQL_ID       '||' '|| 'PLAN_HASH_VALUE' || ' ' || 'SQL_HANDLE                    ' || ' ' || 'PLAN_NAME');
+  dbms_output.put_line('-------------'||' '|| '---------------' || ' ' || '------------------------------' || ' ' || '--------------------------------');
+  FOR a IN (SELECT sql_handle, plan_name, TRIM(SUBSTR(g.plan_table_output, INSTR(g.plan_table_output, ':') +1)) plan_hash_value, sql_text
+            FROM  (
+                  SELECT t.*, c.sql_handle, c.plan_name, c.sql_text FROM dba_sql_plan_baselines c, table(dbms_xplan.DISPLAY_SQL_PLAN_BASELINE(c.sql_handle, c.plan_name)) t
+                  where c.sql_handle = '&sql_handle'
+                  ) g
+            WHERE plan_table_output LIKE 'Plan hash value%'
+          ) LOOP
+    v_num := TO_NUMBER(sys.utl_raw.reverse(sys.UTL_RAW.SUBSTR(sys.dbms_crypto.hash(src => UTL_I18N.string_to_raw(a.sql_text || chr(0),'AL32UTF8'), typ => 2),9,4)) || sys.UTL_RAW.reverse(sys.UTL_RAW.SUBSTR(sys.dbms_crypto.hash(src => UTL_I18N.string_to_raw(a.sql_text || chr(0),'AL32UTF8'), typ => 2),13,4)),RPAD('x', 16, 'x'));
+    v_sqlid := '';
+    FOR i IN 0 .. FLOOR(LN(v_num) / LN(32))
+    LOOP
+        v_sqlid := SUBSTR('0123456789abcdfghjkmnpqrstuvwxyz',FLOOR(MOD(v_num / POWER(32, i), 32)) + 1,1) || v_sqlid;
+    END LOOP;
+    dbms_output.put_line(v_sqlid ||' ' || rpad(a.plan_hash_value,15) || ' ' || rpad(a.sql_handle,30) ||  ' ' || rpad(a.plan_name,30));
+END LOOP;
+END;
+/
