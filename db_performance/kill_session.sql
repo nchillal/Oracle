33ffb2865ba02e44dfce19128a3bf88ad8d9f63c
Nagendra Chillale <nagendra.chillale@yahoo.com>
2017-12-08 14:31:42 -0800

    Added inst_id for kill

--- db_performance/kill_session.sql
+++ db_performance/kill_session.sql
@@ -1,36 +1,36 @@
-SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||Chr(39)||' immediate;'
-FROM    v$session
+SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||',@'||INST_ID||Chr(39)||' IMMEDIATE;'
+FROM    gv$session
 WHERE   sid=&sid
 /
 
-SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||Chr(39)||' immediate;'
-FROM    v$session
+SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||',@'||INST_ID||Chr(39)||' IMMEDIATE;'
+FROM    gv$session
 WHERE   username='&username'
 /
 
-SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||Chr(39)||' immediate;'
-FROM    v$session
+SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||',@'||INST_ID||Chr(39)||' IMMEDIATE;'
+FROM    gv$session
 WHERE   event='&event'
 /
 
-SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||Chr(39)||' immediate;'
-FROM    v$session
+SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||',@'||INST_ID||Chr(39)||' IMMEDIATE;'
+FROM    gv$session
 WHERE   status = 'INACTIVE'
 AND     logon_time < SYSDATE - &hrs/1440
 AND     type <> 'BACKGROUND'
 AND     username = 'CONTENTNR_USER'
 /
 
-SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||Chr(39)||' immediate;'
-FROM    v$session
+SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||',@'||INST_ID||Chr(39)||' IMMEDIATE;'
+FROM    gv$session
 WHERE   status = 'INACTIVE'
 AND     last_call_et > &seconds
 AND     type <> 'BACKGROUND'
 AND     username = 'CONTENTNR_USER'
 /
 
-SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||Chr(39)||' immediate;'
-FROM    v$session
+SELECT  'ALTER SYSTEM KILL SESSION '||Chr(39)||SID||','||SERIAL#||',@'||INST_ID||Chr(39)||' IMMEDIATE;'
+FROM    gv$session
 WHERE   sql_id = '&sql_id'
 AND     sql_child_number = '&child_number'
 /
