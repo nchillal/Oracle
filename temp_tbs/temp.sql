8d2c269a81b4cfb5eab18653588332bbd26d0501
Nagendra Chillale <nagendra.chillale@yahoo.com>
2018-01-08 09:49:26 -0800

    Commit

--- temp_tbs/temp.sql
+++ temp_tbs/temp.sql
@@ -1,6 +1,6 @@
 SELECT username, session_num session_serial#, sql_id, SUM(blocks)*8192/1048576 Temp_MB
 FROM v$tempseg_usage
-where tablespace = '&temp_tablespace'
-group by username, session_num, sql_id
-ORDER BY 3
+WHERE tablespace = '&temp_tablespace'
+GROUP BY username, session_num, sql_id
+ORDER BY 4
 /
