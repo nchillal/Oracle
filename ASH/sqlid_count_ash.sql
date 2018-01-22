8c005bad71172f69e00afb673715462c9e0d8ddf
Nagendra Chillale <nagendra.chillale@yahoo.com>
2018-01-22 13:09:23 -0800

    Added username

--- ASH/sqlid_count_ash.sql
+++ ASH/sqlid_count_ash.sql
@@ -1,23 +1,25 @@
 SELECT  *
 FROM    (
-        SELECT    sql_id, sql_child_number, COUNT(*)
-        FROM      v$active_session_history
-        WHERE     sample_time > TO_DATE('&start_datetime','MMDDYYHH24MI')
-        AND       sample_time < TO_DATE('&end_datetime','MMDDYYHH24MI')
-        AND       user_id > 0
-        GROUP BY  sql_id, sql_child_number
+        SELECT    username, sql_id, sql_child_number, COUNT(*)
+        FROM      v$active_session_history ash, dba_users du
+        WHERE     ash.user_id = du.user_id
+        AND       sample_time > TO_DATE('&start_datetime','DDMMRRHH24MI')
+        AND       sample_time < TO_DATE('&end_datetime','DDMMRRHH24MI')
+        AND       ash.user_id > 0
+        GROUP BY  username, sql_id, sql_child_number
         ORDER BY  COUNT(*) DESC
         )
 WHERE   rownum <= 10;
 
 SELECT  *
 FROM    (
-        SELECT    sql_id,sql_child_number, COUNT(*)
-        FROM      dba_hist_active_sess_history
-        WHERE     sample_time > TO_DATE('&start_datetime','MMDDYYHH24MI')
-        AND       sample_time < TO_DATE('&end_datetime','MMDDYYHH24MI')
-        AND       user_id > 0
-        GROUP BY  sql_id, sql_child_number
+        SELECT    username, sql_id,sql_child_number, COUNT(*)
+        FROM      dba_hist_active_sess_history ash, dba_users du
+        WHERE     ash.user_id = du.user_id
+        AND       sample_time > TO_DATE('&start_datetime','DDMMRRHH24MI')
+        AND       sample_time < TO_DATE('&end_datetime','DDMMRRHH24MI')
+        AND       ash.user_id > 0
+        GROUP BY  username, sql_id, sql_child_number
         ORDER BY  COUNT(*)
         )
 WHERE   rownum <= 10;
