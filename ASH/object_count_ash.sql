c52fa3abd5a5f8c29acb77da606a8b010ad359c5
Nagendra Chillale <nagendra.chillale@yahoo.com>
2018-01-22 13:44:36 -0800

    Added username

--- ASH/object_count_ash.sql
+++ ASH/object_count_ash.sql
@@ -1,21 +1,23 @@
 COLUMN object_name FORMAT a30
 
-SELECT    object_name, object_type, COUNT(*)
-FROM      v$active_session_history ash, dba_objects do
-WHERE     sample_time > TO_DATE('&start_datetime','MMDDYYHH24MI')
-AND       sample_time < TO_DATE('&end_datetime','MMDDYYHH24MI')
-AND       user_id > 0
+SELECT    username, object_name, object_type, COUNT(*)
+FROM      v$active_session_history ash, dba_objects do, dba_users du
+WHERE     ash.user_id = du.user_id
+AND       sample_time > TO_DATE('&start_datetime','DDMMRRHH24MI')
+AND       sample_time < TO_DATE('&end_datetime','DDMMRRHH24MI')
+AND       ash.user_id > 0
 AND       ash.current_obj# = do.object_id
 HAVING    COUNT(*) > 10
-GROUP BY  object_name, object_type
+GROUP BY  username, object_name, object_type
 ORDER BY  COUNT(*);
 
-SELECT    object_name, object_type, COUNT(*)
-FROM      dba_hist_active_sess_history ash, dba_objects do
-WHERE     sample_time > TO_DATE('&start_datetime','MMDDYYHH24MI')
-AND       sample_time < TO_DATE('&end_datetime','MMDDYYHH24MI')
-AND       user_id > 0
+SELECT    username, object_name, object_type, COUNT(*)
+FROM      dba_hist_active_sess_history ash, dba_objects do, dba_users du
+WHERE     ash.user_id = du.user_id
+AND       sample_time > TO_DATE('&start_datetime','DDMMRRHH24MI')
+AND       sample_time < TO_DATE('&end_datetime','DDMMRRHH24MI')
+AND       ash.user_id > 0
 AND       ash.current_obj# = do.object_id
 HAVING    COUNT(*) > 10
-GROUP BY  object_name, object_type
+GROUP BY  username, object_name, object_type
 ORDER BY  COUNT(*);
