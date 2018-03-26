e7cce472394ea804922aae813ea25db9e2aebea9
Nagendra Chillale <nagendra.chillale@yahoo.com>
2018-03-25 21:12:58 -0700

    Changes

--- ASM/asm_disk_mount_status.sql
+++ ASM/asm_disk_mount_status.sql
@@ -1,9 +1,8 @@
 COLUMN path FORMAT a40
 COLUMN name FORMAT a15
-COLUMN "Disk Name" FORMAT a20
 COLUMN failgroup FORMAT a15
 
-SELECT    group_number, disk_number, failgroup,name "Disk Name", mount_status, path
+SELECT    group_number, disk_number, failgroup,name "Disk Name", mount_status "Mount Status", state, mode_status "Mode Status", path
 FROM      v$asm_disk
 ORDER BY  failgroup, name, path;
 
