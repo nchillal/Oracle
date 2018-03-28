215fb1abbb3318a0c2218fc7c4011630fe2fb765
Nagendra Chillale <nagendra.chillale@yahoo.com>
2018-03-27 21:03:42 -0700

    Changes

--- db_performance/ASH/sqlid_bind_info.sql
+++ db_performance/ASH/sqlid_bind_info.sql
@@ -1,9 +1,18 @@
 --Corresponding Equivalent AWR table is DBA_HIST_SQLBIND.
-SELECT    sql_id,
-          t.sql_text sql_text,
+COLUMN sql_text FORMAT a120
+COLUMN bind_name FORMAT a10
+COLUMN bind_value FORMAT a25
+SELECT    t.sql_text sql_text,
           b.name bind_name,
           b.value_string bind_value
 FROM      v$sql t
 JOIN      v$sql_bind_capture b using (sql_id)
 WHERE     b.value_string is not null
-AND       sql_id='&sqlid';
+AND       sql_id = '&sql_id';
+
+BREAK ON snap_id SKIP 1
+SELECT    snap_id, name bind_name, value_string bind_value
+FROM      dba_hist_sqlbind
+WHERE     sql_id = '&sql_id'
+AND       snap_id BETWEEN '&begin_snap' AND '&end_snap'
+ORDER BY  snap_id, bind_name;
