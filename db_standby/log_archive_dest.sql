135ab727cd678fbd4ae1739e87ab4980e7d6a551
Nagendra Chillale <nagendra.chillale@yahoo.com>
2018-03-29 10:19:25 -0700

    Changes

--- db_standby/log_archive_dest.sql
+++ db_standby/log_archive_dest.sql
@@ -1,10 +1,10 @@
-SET linesize 155 pagesize 200
+SET linesize 158 pagesize 200
 COLUMN name FORMAT a30
 COLUMN value FORMAT a120
 
 SELECT  name, value
 FROM    v$spparameter
-WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=*')
+WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=*|SERVICE=*')
 UNION
 SELECT  name, value
 FROM    v$spparameter
@@ -12,13 +12,13 @@ WHERE   REGEXP_LIKE(name, 'log_archive_dest_state_+\d')
 AND     name IN (
                   SELECT  'log_archive_dest_state_'||REGEXP_SUBSTR(name, '[0-9]+')
                   FROM    v$spparameter
-                  WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=*')
+                  WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=*|SERVICE=*')
 )
 /
 
 SELECT  name, value
 FROM    v$spparameter
-WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=&service_name')
+WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=&&service_name|SERVICE=&&service_name')
 UNION
 SELECT  name, value
 FROM    v$spparameter
@@ -26,6 +26,6 @@ WHERE   REGEXP_LIKE(name, 'log_archive_dest_state_+\d')
 AND     name IN (
                   SELECT  'log_archive_dest_state_'||REGEXP_SUBSTR(name, '[0-9]+')
                   FROM    v$spparameter
-                  WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=&service_name')
+                  WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=&&service_name|SERVICE=&&service_name')
 )
 /
