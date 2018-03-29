cefd81a00b5b2f4bff9e10150033f4bbed5d7158
Nagendra Chillale <nagendra.chillale@yahoo.com>
2018-03-28 18:53:29 -0700

    Filter value which is NULL

--- db_standby/lag_check.sql
+++ db_standby/lag_check.sql
@@ -3,4 +3,5 @@ BREAK ON inst_id SKIP 1
 SELECT    inst_id, name, value, time_computed
 FROM      gv$dataguard_stats
 WHERE     name IN ('transport lag', 'apply lag')
+AND       value IS NOT NULL
 ORDER BY  inst_id;
