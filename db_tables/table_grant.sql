8f733b23c3593120ca427294e12496c8f9bee984
Nagendra Chillale <12175246+nchillal@users.noreply.github.com>
2018-04-16 14:14:11 -0700

    Changes

--- db_tables/table_grant.sql
+++ db_tables/table_grant.sql
@@ -1,5 +1,13 @@
+-- Gets grants given for a specific table.
 SET LINES 260 PAGES 2000
 SELECT  'GRANT '||privilege||' ON '||owner||'.'||table_name||' TO '|| grantee||';' "TABLE GRANTS"
+FROM    dba_tab_privs
+WHERE   table_name LIKE '&table_name'
+AND     owner='&table_owner';
+
+-- Grant Revokes On a Table.
+SET LINES 260 PAGES 2000
+SELECT  'REVOKE '||privilege||' ON '||owner||'.'||table_name||' FROM '|| grantee||';' "TABLE GRANTS"
 FROM    dba_tab_privs
 WHERE   table_name LIKE '&table_name'
 AND     owner='&table_owner';
