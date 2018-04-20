af61d27a8e4ccd6ca3d630a2eb7d4ee5f55f59d8
Nagendra Chillale <12175246+nchillal@users.noreply.github.com>
2018-04-19 20:45:03 -0700

    Changes

--- db_performance/purge_plan_sqlid.sql
+++ db_performance/purge_plan_sqlid.sql
@@ -2,4 +2,8 @@ SELECT 	first_load_time, address, hash_value, plan_hash_value
 FROM 	  v$sqlarea
 WHERE 	sql_id='&sql_id';
 
+SELECT 	first_load_time, last_load_time, address, hash_value, plan_hash_value
+FROM 	  v$sql
+WHERE 	sql_id='&sql_id';
+
 EXEC DBMS_SHARED_POOL.purge('&address,&hash_value', 'C');
