9a6224c4c6aab2130d63c95c6611266f65a9a580
Nagendra Chillale <12175246+nchillal@users.noreply.github.com>
2018-05-06 18:52:16 -0700

    Changes

--- undo_tbs/undo_generated.sql
+++ undo_tbs/undo_generated.sql
@@ -1,6 +1,3 @@
-BREAK ON begin_time SKIP 1
-COMPUTE SUM LABEL 'TOTAL' OF UNDO_SIZE_MB ON begin_time
-
 SELECT  a.begin_time "BEGIN_TIME", a.instance_number "INSTANCE_NUMBER", ROUND((a.undoblks * b.value)/1024/1024, 2) "UNDO_SIZE_MB"
 FROM    (
           SELECT    instance_number, TO_CHAR(begin_time, 'DD-MON-YYYY') "BEGIN_TIME", SUM(undoblks) "UNDOBLKS"
@@ -11,9 +8,6 @@ FROM    (
         (SELECT value FROM v$parameter WHERE name='db_block_size') b
 ORDER BY 1,2,3;
 
-CLEAR COMPUTE
-
-BREAK ON day ON hour_min SKIP 1
 SELECT  a.day "DAY", a.hour_min "HOUR_MIN", a.instance_number "INSTANCE_NUMBER", ROUND((a.undoblks * b.value)/1024/1024, 2) "UNDO_SIZE_MB"
 FROM    (
           SELECT    instance_number, TO_CHAR(begin_time, 'DD-MON-YYYY') "DAY", TO_CHAR(begin_time, 'HH24:MI') "HOUR_MIN", SUM(undoblks) "UNDOBLKS"
@@ -24,7 +18,6 @@ FROM    (
         (SELECT value FROM v$parameter WHERE name='db_block_size') b
 ORDER BY 1,2,3;
 
-BREAK ON DAY ON HOUR_MIN SKIP 1
 SELECT  a.day "DAY", a.hour_min "HOUR_MIN", a.inst_id "INSTANCE_NUMBER", ROUND((a.undoblks * b.value)/1024/1024, 2) "UNDO_SIZE_MB"
 FROM    (
           SELECT inst_id, TO_CHAR(begin_time, 'DD-MON-YYYY') "DAY", TO_CHAR(begin_time, 'HH24:MI') "HOUR_MIN", SUM(undoblks) "UNDOBLKS"
