81466eee994da5423e67535f4dc1f90f03543c54
Nagendra Chillale <12175246+nchillal@users.noreply.github.com>
2018-06-25 22:39:15 -0700

    Changes

--- undo_tbs/undo_segment_status.sql
+++ undo_tbs/undo_segment_status.sql
@@ -1,6 +1,6 @@
 SELECT    tablespace_name,
-          ROUND(SUM(case when status = 'UNEXPIRED' then bytes else 0 end) / (1024 * 1024 * 1024), 2.2) unexp_MB ,
-          ROUND(SUM(case when status = 'EXPIRED' then bytes else 0end) / (1024 * 1024 * 1024), 2.2) exp_MB ,
-          ROUND(SUM(case when status = 'ACTIVE' then bytes else 0 end) / (1024 * 1024 * 1024), 2.2) act_MB
+          ROUND(SUM(CASE WHEN status = 'UNEXPIRED' THEN bytes ELSE 0 END) / (1024 * 1024 * 1024), 2.2) unexp_MB ,
+          ROUND(SUM(CASE WHEN status = 'EXPIRED' THEN bytes ELSE 0 END) / (1024 * 1024 * 1024), 2.2) exp_MB ,
+          ROUND(SUM(CASE WHEN status = 'ACTIVE' THEN bytes else 0 END) / (1024 * 1024 * 1024), 2.2) act_MB
 FROM      dba_undo_extents
 GROUP BY  tablespace_name;
