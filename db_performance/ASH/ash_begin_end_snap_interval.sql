358f882773c9046f655e86680a1ec980fb867c91
Nagendra Chillale <12175246+nchillal@users.noreply.github.com>
2018-06-28 07:36:07 -0700

    Changes

--- db_performance/ASH/ash_begin_end_snap_interval.sql
+++ db_performance/ASH/ash_begin_end_snap_interval.sql
@@ -4,8 +4,9 @@ SELECT    begin_snap||' ---> '||end_snap "BEGIN_END_SNAPID",
 FROM
 (
           SELECT  snap_id "BEGIN_SNAP",
-          LEAD(SNAP_ID, 1) OVER(ORDER BY snap_id) "END_SNAP",
-          TO_CHAR(BEGIN_INTERVAL_TIME, 'DDMMRRHH24MI') "BEGIN_TIME",
-          LEAD(TO_CHAR(BEGIN_INTERVAL_TIME, 'DDMMRRHH24MI'), 1) OVER(ORDER BY snap_id) "END_TIME"
+                  LEAD(SNAP_ID, 1) OVER(ORDER BY snap_id) "END_SNAP",
+                  TO_CHAR(BEGIN_INTERVAL_TIME, 'DDMMRRHH24MI') "BEGIN_TIME",
+                  LEAD(TO_CHAR(BEGIN_INTERVAL_TIME, 'DDMMRRHH24MI'), 1) OVER(ORDER BY snap_id) "END_TIME"
           FROM    dba_hist_snapshot
+          WHERE   instance_number = 1
 );
