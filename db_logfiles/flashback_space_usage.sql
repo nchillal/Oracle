55ce9191dbbcf1eee1abe65e93224f74885b2cff
Nagendra Chillale <nchillal@apple.com>
2018-06-27 21:01:20 -0700

    Changes

--- db_logfiles/flashback_space_usage.sql
+++ db_logfiles/flashback_space_usage.sql
@@ -5,4 +5,24 @@ SELECT  file_type,
         number_of_files,
         percent_space_reclaimable * p.value / 100 / 1024 / 1024 "MB Reclaimable"
 FROM    v$flash_recovery_area_usage f, v$parameter p
-WHERE   p.name='db_recovery_file_dest_size'; 
+WHERE   p.name='db_recovery_file_dest_size';
+
+
+DECLARE
+    pcntrclm number;
+    pcntused number;
+    recdestsize number;
+    totrcml number;
+    totused number;
+BEGIN
+    SELECT SUM(percent_space_reclaimable) INTO pcntrclm FROM v$flash_recovery_area_usage;
+    SELECT SUM(percent_space_used) INTO pcntused from v$flash_recovery_area_usage;
+    SELECT value INTO recdestsize FROM v$parameter where name='db_recovery_file_dest_size';
+    totrcml := pcntrclm * recdestsize / 100 / 1024 / 1024;
+    totused := pcntused * recdestsize / 100 / 1024 / 1024;
+    DBMS_OUTPUT.PUT_LINE('Total FRA space: '||to_char(recdestsize/1024/1024)||' Mb');
+    DBMS_OUTPUT.PUT_LINE('Total used: '||to_char(totused)||' Mb');
+    DBMS_OUTPUT.PUT_LINE('Total free: '||to_char(recdestsize/1024/1024-totused)||' Mb');
+    DBMS_OUTPUT.PUT_LINE('Total reclaimable: '||to_char(totrcml)||' Mb');
+END;
+/
--- db_streams/streams_config.sql
+++ db_streams/streams_config.sql
@@ -1,7 +1,9 @@
-BREAK ON capture_name ON queue_name ON queue_table ON propagation_name ON schema_name SKIP 1
-
-SELECT    capture_name, queue_name, queue_table, propagation_name, schema_name, object_name
+BREAK ON capture_name ON queue_name ON queue_table ON propagation_name ON destination_dblink ON schema_name SKIP 1
+SELECT    capture_name, queue_name, queue_table, propagation_name, destination_dblink, schema_name, object_name
 FROM      dba_capture dc, dba_queues du, dba_propagation dp, dba_streams_rules dsr
 WHERE     dc.queue_name = du.name
 AND       dc.queue_name = dp.source_queue_name
-AND       dc.capture_name = dsr.streams_name;
+AND       dc.capture_name = dsr.streams_name
+ORDER BY  schema_name;
+
+SELECT distinct schema_name FROM dba_
