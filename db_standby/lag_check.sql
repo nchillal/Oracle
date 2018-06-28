4956670ec19ba85f61f2bf06a966514dfa7bf331
Nagendra Chillale <12175246+nchillal@users.noreply.github.com>
2018-06-27 17:36:56 -0700

    Changes

--- db_standby/lag_check.sql
+++ db_standby/lag_check.sql
@@ -5,3 +5,15 @@ FROM      gv$dataguard_stats
 WHERE     name IN ('transport lag', 'apply lag')
 AND       value IS NOT NULL
 ORDER BY  inst_id;
+
+SELECT    ROUND((sysdate - a.NEXT_TIME)*24*60) as "Backlog MIN",
+          m.sequence# "Sequence Applied",
+          a.thread#,
+          m.process,
+          m.block# "Block",
+          m.status,
+          a.dest_id
+FROM      v$archived_log a, (SELECT process, block#, sequence#, status FROM v$managed_standby WHERE process LIKE '%MRP%') m
+WHERE     a.sequence#=(m.sequence#-1)
+AND       a.status='A'
+/
