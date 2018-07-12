5c58b07eb6e60a3bcac4ec09eb9d0cb2cb155c48
Nagendra Chillale <12175246+nchillal@users.noreply.github.com>
2018-07-11 21:46:00 -0700

    Changes

--- db_streams/streams_config.sql
+++ db_streams/streams_config.sql
@@ -1,7 +1,10 @@
 COLUMN destination_dblink FORMAT a20
-column CAPTURE_RULE_SET for a20
-column CAPTURE_NAME for a20
+COLUMN capture_rule_set FORMAT a20
+COLUMN capture_name FORMAT a20
+COLUMN apply_tag FORMAT a10
+
 BREAK ON capture_name ON queue_name ON queue_table ON propagation_name ON destination_dblink ON schema_name SKIP 1
+
 SELECT    capture_name, dc.rule_set_name capture_rule_set, queue_name, queue_table, propagation_name, dp.rule_set_name propgation_rule_set, destination_dblink, schema_name, object_name
 FROM      dba_capture dc, dba_queues du, dba_propagation dp, dba_streams_rules dsr
 WHERE     dc.queue_name = du.name
@@ -9,6 +12,5 @@ AND       dc.queue_name = dp.source_queue_name
 AND       dc.capture_name = dsr.streams_name
 ORDER BY  schema_name;
 
-COLUMN apply_tag FOR a10
 SELECT    apply_name, queue_name, rule_set_name, apply_user, apply_tag
 FROM      dba_apply;
--- print_table.sql
+++ print_table.sql
@@ -2,10 +2,7 @@
 --set serveroutput on
 --SQL> exec print_table('select * from dba_users where username = ''SYSTEM''');
 --PROCEDURE TO CREATE COLUMN DISPLAY
-create or replace
-procedure print_table
-( p_query in varchar2,
-p_date_fmt in varchar2 default 'dd-mon-yyyy hh24:mi:ss')
+create or replace procedure print_table (p_query in varchar2, p_date_fmt in varchar2 default 'dd-mon-yyyy hh24:mi:ss')
 -- this utility is designed to be installed ONCE in a database and used
 -- by all. Also, it is nice to have roles enabled so that queries by
 -- DBA's that use a role to gain access to the DBA_* views still work
@@ -28,13 +25,11 @@ is
 begin
 if ( upper(l_cs) not in ( 'FORCE','SIMILAR' ))
 then
-execute immediate
-'alter session set cursor_sharing=exact';
+    execute immediate 'alter session set cursor_sharing=exact';
 end if;
 if ( p_date_fmt is not null )
 then
-execute immediate
-'alter session set nls_date_format=''' || l_date_fmt || '''';
+    execute immediate 'alter session set nls_date_format=''' || l_date_fmt || '''';
 end if;
 dbms_sql.close_cursor(l_theCursor);
 end restore;
@@ -44,28 +39,22 @@ begin
 -- we save the date current sessions date format and then use
 -- the one with the date and time. Passing in NULL will cause
 -- this routine just to use the current date format
-if ( p_date_fmt is not null )
+if (p_date_fmt is not null)
 then
-select sys_context( 'userenv', 'nls_date_format' )
-into l_date_fmt
-from dual;
-
-execute immediate
-'alter session set nls_date_format=''' || p_date_fmt || '''';
+    select sys_context( 'userenv', 'nls_date_format' ) into l_date_fmt from dual;
+    execute immediate 'alter session set nls_date_format=''' || p_date_fmt || '''';
 end if;
 
 -- to be bind variable friendly on this ad-hoc queries, we
 -- look to see if cursor sharing is already set to FORCE or
 -- similar, if not, set it so when we parse — literals
 -- are replaced with binds
-if ( dbms_utility.get_parameter_value
-( 'cursor_sharing', l_status, l_cs ) = 1 )
+if (dbms_utility.get_parameter_value( 'cursor_sharing', l_status, l_cs ) = 1)
 then
-if ( upper(l_cs) not in ('FORCE','SIMILAR'))
-then
-execute immediate
-'alter session set cursor_sharing=force';
-end if;
+    if ( upper(l_cs) not in ('FORCE','SIMILAR'))
+    then
+        execute immediate 'alter session set cursor_sharing=force';
+    end if;
 end if;
 
 -- parse and describe the query sent to us. we need
@@ -76,11 +65,11 @@ dbms_sql.describe_columns( l_theCursor, l_colCnt, l_descTbl );
 -- define all columns to be cast to varchar2's, we
 -- are just printing them out
 for i in 1 .. l_colCnt loop
-if ( l_descTbl(i).col_type not in ( 113 ) )
-then
-dbms_sql.define_column
-(l_theCursor, i, l_columnValue, 4000);
-end if;
+    if ( l_descTbl(i).col_type not in ( 113 ) )
+    then
+        dbms_sql.define_column
+        (l_theCursor, i, l_columnValue, 4000);
+    end if;
 end loop;
 
 -- execute the query, so we can fetch
@@ -89,20 +78,21 @@ l_status := dbms_sql.execute(l_theCursor);
 -- loop and print out each column on a separate line
 -- bear in mind that dbms_output only prints 255 characters/line
 -- so we'll only see the first 200 characters by my design…
+dbms_output.put_line(chr(13));
 dbms_output.put_line( '—————–' );
-while ( dbms_sql.fetch_rows(l_theCursor) > 0 )
+while (dbms_sql.fetch_rows(l_theCursor) > 0)
 loop
-for i in 1 .. l_colCnt loop
-if ( l_descTbl(i).col_type not in ( 113 ) )
-then
-dbms_sql.column_value
-( l_theCursor, i, l_columnValue );
-dbms_output.put_line
-( rpad( l_descTbl(i).col_name, 30 )
-|| ': ' ||
-substr( l_columnValue, 1, 200 ) );
-end if;
-end loop;
+    for i in 1 .. l_colCnt loop
+        if ( l_descTbl(i).col_type not in ( 113 ) )
+        then
+            dbms_sql.column_value
+            ( l_theCursor, i, l_columnValue );
+            dbms_output.put_line
+            ( rpad( l_descTbl(i).col_name, 30 )
+            || ': ' ||
+            substr( l_columnValue, 1, 200 ) );
+        end if;
+    end loop;
 dbms_output.put_line( '—————–' );
 end loop;
 
