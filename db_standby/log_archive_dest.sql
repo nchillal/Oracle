52c9cce4535c4259c95fc173b5d877138e72b4c3
Nagendra Chillale <nchillal@apple.com>
2018-07-28 13:41:44 -0700

    Changed from spfile to pfile view

--- db_standby/log_archive_dest.sql
+++ db_standby/log_archive_dest.sql
@@ -3,29 +3,29 @@ COLUMN name FORMAT a30
 COLUMN value FORMAT a120
 
 SELECT  name, value
-FROM    v$spparameter
+FROM    v$parameter
 WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=*|SERVICE=*')
 UNION
 SELECT  name, value
-FROM    v$spparameter
+FROM    v$parameter
 WHERE   REGEXP_LIKE(name, 'log_archive_dest_state_+\d')
 AND     name IN (
                   SELECT  'log_archive_dest_state_'||REGEXP_SUBSTR(name, '[0-9]+')
-                  FROM    v$spparameter
+                  FROM    v$parameter
                   WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=*|SERVICE=*')
 )
 /
 
 SELECT  name, value
-FROM    v$spparameter
+FROM    v$parameter
 WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=&&service_name|SERVICE=&&service_name')
 UNION
 SELECT  name, value
-FROM    v$spparameter
+FROM    v$parameter
 WHERE   REGEXP_LIKE(name, 'log_archive_dest_state_+\d')
 AND     name IN (
                   SELECT  'log_archive_dest_state_'||REGEXP_SUBSTR(name, '[0-9]+')
-                  FROM    v$spparameter
+                  FROM    v$parameter
                   WHERE   REGEXP_LIKE(name, 'log_archive_dest_\d+') AND REGEXP_LIKE (value, 'service=&&service_name|SERVICE=&&service_name')
 )
 /
