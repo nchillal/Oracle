e3b21c3e705b5fc67670ac9f3356523d6e138b89
Nagendra Chillale <nchillal@apple.com>
2018-07-30 11:51:44 -0700

    Get locks from ASH

--- db_performance/glocks.sql
+++ db_performance/glocks.sql
@@ -13,3 +13,13 @@ SELECT    inst_id, sid||','||serial# "SID_SERIAL#", sql_id, blocking_session, bl
 FROM      gv$session
 WHERE     event ='&event_name'
 AND       blocking_session IS NOT NULL;
+
+SELECT    session_id||','||session_serial# "SID_SERIAL#", sql_id, blocking_session, blocking_session_status, session_state, event
+FROM      v$active_session_history
+WHERE     blocking_session IS NOT NULL
+AND       sample_time > SYSDATE - INTERVAL '&minutes' MINUTE;
+
+SELECT    session_id||','||session_serial# "SID_SERIAL#", sql_id, blocking_session, blocking_session_status, session_state, event
+FROM      dba_hist_active_sess_history
+WHERE     blocking_session IS NOT NULL
+AND       snap_id BETWEEN &begin_snap AND &end_snap;
