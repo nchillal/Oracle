6c0e6fe818e134568f1600594ed0d83f19e81cff
Nagendra Chillale <nchillal@apple.com>
2018-08-02 11:01:33 -0700

    Oracle Streams Drop Rule

---
+++ db_streams/drop_rule.sql
@@ -0,0 +1,4 @@
+SELECT    'EXEC DBMS_PROPAGATION_ADM.ALTER_'||streams_type||'('||DECODE(streams_type, 'APPLY', 'apply_name', 'PROPAGATION', 'propagation_name')||' => '||''''||rule_name||''''||', remove_rule_set => true);' "DDL Remove Rule"
+FROM      dba_streams_rules
+WHERE     rule_name IN (SELECT RULE_NAME FROM dba_streams_table_rules WHERE TABLE_OWNER = '&table_owner')
+AND       streams_type = '&streams_type';
