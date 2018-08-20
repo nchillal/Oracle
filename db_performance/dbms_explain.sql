401718a99b35bbec33f9be9e26cb8452bbc16a50
Nagendra Chillale <nchillal@apple.com>
2018-08-20 14:20:43 -0700

    Changes

--- db_performance/dbms_explain.sql
+++ db_performance/dbms_explain.sql
@@ -5,13 +5,10 @@ COLUMN plan_table_output FORMAT a160
 SELECT * FROM table(DBMS_XPLAN.DISPLAY(FORMAT => 'ALL'));
 
 -- Display current execution plan of sql_id.
-SELECT  *
-FROM    table(DBMS_XPLAN.display_cursor('&sql_id', &child_number, FORMAT => 'ALL'));
+SELECT * FROM table(DBMS_XPLAN.display_cursor('&sql_id', &child_number, FORMAT => 'ALL'));
 
 -- Display all execution plan for a sql_id.
-SELECT  *
-FROM    table(DBMS_XPLAN.display_awr('&sql_id', FORMAT => 'ALL'));
+SELECT * FROM table(DBMS_XPLAN.display_awr('&sql_id', FORMAT => 'ALL'));
 
 -- Display execution plan of a sql plan baseline. Pass sql_handle from dba_sql_plan_baselines
-SELECT *
-FROM TABLE(DBMS_XPLAN.DISPLAY_SQL_PLAN_BASELINE('&sql_handle', FORMAT => 'ALL'));
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_SQL_PLAN_BASELINE('&sql_handle', FORMAT => 'ALL'));
