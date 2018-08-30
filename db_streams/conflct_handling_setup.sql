705ed745f22573bd487953f9c7a1f781b63ad177
Nagendra Chillale <nchillal@apple.com>
2018-08-29 20:19:20 -0700

    Changes

--- db_streams/conflct_handling_setup.sql
+++ db_streams/conflct_handling_setup.sql
@@ -1,25 +1,25 @@
 EXEC strmadmin.gsd_streams_adm.set_tag(tag => HEXTORAW('17'));
 
-SET serveroutput ON;
+SET SERVEROUTPUT ON;
 DECLARE
     LM_Column VARCHAR2(5000) DEFAULT NULL;
     LM_COUNT NUMBER;
 CURSOR cur IS SELECT at.table_name, at.owner FROM all_tables at WHERE 1=1 AND owner IN ('&schema_owner') AND at.table_name NOT LIKE 'STR%LAG%MON%' ORDER BY owner;
 BEGIN
     DBMS_OUTPUT.ENABLE(1000000);
-    dbms_output.put_line(chr(10));
+    DBMS_OUTPUT.PUT_LINE(CHR(10));
     FOR rec IN cur
     LOOP
         --dbms_output.put_line('The table name is ' || rec.table_name);
         --dbms_output.put_line('The owner name is ' || rec.owner);
         LM_Column := null;
-        dbms_output.put_line(chr(10));
-        dbms_output.put_line('declare');
-        dbms_output.put_line('cols dbms_utility.name_array;');
-        dbms_output.put_line('begin');
+        DBMS_OUTPUT.PUT_LINE(chr(10));
+        DBMS_OUTPUT.PUT_LINE('declare');
+        DBMS_OUTPUT.PUT_LINE('cols dbms_utility.name_array;');
+        DBMS_OUTPUT.PUT_LINE('begin');
         FOR i IN (SELECT owner, table_name, column_name, column_id FROM all_tab_columns atc WHERE atc.table_name=rec.table_name AND atc.owner=rec.owner ORDER BY column_id)
         LOOP
-            dbms_output.put_line('cols('||i.COLUMN_ID||') :='''||i.COLUMN_NAME||''';');
+            DBMS_OUTPUT.PUT_LINE('cols('||i.column_id||') :='''||i.column_name||''';');
         END LOOP;
 
         BEGIN
@@ -31,14 +31,14 @@ BEGIN
             SELECT column_name INTO LM_Column FROM all_tab_columns atc WHERE atc.table_name=rec.table_name AND atc.owner=rec.owner AND atc.column_name IN ('LASTMODIFIED', 'LAST_MODIFIED');
         END;
 
-        dbms_output.put_line('dbms_apply_adm.set_update_conflict_handler(');
-        dbms_output.put_line('object_name         => '''||rec.owner||'.'||rec.table_name||''',');
-        dbms_output.put_line('method_name         => ''MAXIMUM'',');
-        dbms_output.put_line('resolution_column   => '''||LM_Column||''',');
-        dbms_output.put_line('column_list         => cols ');
-        dbms_output.put_line(');');
-        dbms_output.put_line('end;');
-        dbms_output.put_line('/');
+        DBMS_OUTPUT.PUT_LINE('dbms_apply_adm.set_update_conflict_handler(');
+        DBMS_OUTPUT.PUT_LINE('object_name         => '''||rec.owner||'.'||rec.table_name||''',');
+        DBMS_OUTPUT.PUT_LINE('method_name         => ''MAXIMUM'',');
+        DBMS_OUTPUT.PUT_LINE('resolution_column   => '''||LM_Column||''',');
+        DBMS_OUTPUT.PUT_LINE('column_list         => cols ');
+        DBMS_OUTPUT.PUT_LINE(');');
+        DBMS_OUTPUT.PUT_LINE('end;');
+        DBMS_OUTPUT.PUT_LINE('/');
     END LOOP;
 EXCEPTION
     WHEN OTHERS THEN
