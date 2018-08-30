7dc4bc23356fddc64d5729a586ff222fea86922f
Nagendra Chillale <nchillal@apple.com>
2018-08-29 20:46:23 -0700

    Changes

--- db_indexes/objind.sql
+++ db_indexes/objind.sql
@@ -4,37 +4,30 @@ DECLARE
     -- Create bind variable for storing table_name
     v_table_owner VARCHAR2(30) := '&table_owner';
     v_table_name VARCHAR2(30) := '&table_name';
-
-    TYPE dbIndexes IS TABLE OF dba_indexes%ROWTYPE;
-    rec1 dbIndexes;
-
-    TYPE dbIndCols IS TABLE OF dba_ind_columns%ROWTYPE;
-    rec2 dbIndCols;
-
 BEGIN
     -- List indexes defined on the table.
-    SELECT  * BULK COLLECT INTO rec1
-    FROM    dba_indexes
-    WHERE   table_name=v_table_name
-    AND     owner=v_table_owner;
-
     DBMS_OUTPUT.PUT_LINE(CHR(13));
 
-    FOR row IN rec1.FIRST..rec1.LAST
+    FOR row IN  (
+                SELECT  index_name, tablespace_name, last_analyzed, index_type, uniqueness, status, partitioned
+                FROM    dba_indexes
+                WHERE   table_name=v_table_name
+                AND     owner=v_table_owner
+                )
     LOOP
-        DBMS_OUTPUT.PUT_LINE(RPAD(rec1(row).index_name, 30)||RPAD(rec1(row).tablespace_name, 30)||RPAD(rec1(row).last_analyzed, 30)||RPAD(rec1(row).index_type, 15)||RPAD(rec1(row).uniqueness, 15)||RPAD(rec1(row).status, 15)||RPAD(rec1(row).partitioned, 30));
+        DBMS_OUTPUT.PUT_LINE(RPAD(row.index_name, 30)||RPAD(row.tablespace_name, 30)||RPAD(row.last_analyzed, 30)||RPAD(row.index_type, 15)||RPAD(row.uniqueness, 15)||RPAD(row.status, 15)||RPAD(row.partitioned, 30));
     END LOOP;
 
     DBMS_OUTPUT.PUT_LINE(CHR(13));
 
-    SELECT  * BULK COLLECT INTO rec2
-    FROM    dba_ind_columns
-    WHERE   table_name=v_table_name
-    AND     table_owner=v_table_owner;
-
-    FOR row IN rec2.FIRST..rec2.LAST
+    FOR row IN  (
+                SELECT  index_name, column_position, column_name
+                FROM    dba_ind_columns
+                WHERE   table_name=v_table_name
+                AND     table_owner=v_table_owner
+                )
     LOOP
-        DBMS_OUTPUT.PUT_LINE(RPAD(rec2(row).index_name, 30)||RPAD(rec2(row).column_position, 5)||RPAD(rec2(row).column_name, 30));
+        DBMS_OUTPUT.PUT_LINE(RPAD(row.index_name, 30)||RPAD(row.column_position, 5)||RPAD(row.column_name, 30));
     END LOOP;
 
     DBMS_OUTPUT.PUT_LINE(CHR(13));
