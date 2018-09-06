22d29f28d35b7d7a1fb7db80dd5f8f4e88cb1c10
Nagendra Chillale <nchillal@apple.com>
2018-09-06 16:49:45 -0700

    Changes

--- db_streams/streams_config.sql
+++ db_streams/streams_config.sql
@@ -34,9 +34,8 @@ SELECT object_owner, object_name, COUNT(*) FROM dba_apply_conflict_columns GROUP
 -- Capture Lag
 SELECT  capture_name, ((SYSDATE - capture_message_create_time)*86400) LATENCY_SECONDS
 FROM    v$streams_capture
-WHERE EXISTS (SELECT 1 FROM v$database WHERE database_role='PRIMARY');
-
--- Apply Lag
+WHERE EXISTS (SELECT 1 FROM v$database WHERE database_role='PRIMARY')
+UNION
 SELECT  apply_name, NVL( (hwm_time-hwm_message_create_time)*86400, 0) LAG_SEC
 FROM    v$streams_apply_coordinator
 WHERE EXISTS (SELECT 1 FROM v$database WHERE database_role='PRIMARY');
