89fa4158e6610b3f2f1cfddf03a463d967cf59a6
Nagendra Chillale <nchillal@apple.com>
2018-09-22 11:58:09 -0700

    Changes

--- db_performance/spm_sqlid.sql
+++ db_performance/spm_sqlid.sql
@@ -2,8 +2,8 @@ SET SERVEROUTPUT ON
 DECLARE
   l_plans_loaded  PLS_INTEGER;
 BEGIN
-  l_plans_loaded := DBMS_SPM.load_plans_from_cursor_cache(sql_id => '&sql_id', plan_hash_value => &plan_hash_value);
-  DBMS_OUTPUT.put_line('Plans Loaded: ' || l_plans_loaded);
+  l_plans_loaded := DBMS_SPM.LOAD_PLANS_FROM_CURSOR_CACHE(sql_id => '&sql_id', plan_hash_value => &plan_hash_value);
+  DBMS_OUTPUT.PUT_LINE('Plans Loaded: ' || l_plans_loaded);
 END;
 /
 
@@ -19,13 +19,13 @@ FROM    dba_sql_plan_baselines
 WHERE   signature IN (SELECT exact_matching_signature FROM v$sql WHERE sql_id='&SQL_ID');
 
 SELECT *
-FROM   TABLE(DBMS_XPLAN.display_sql_plan_baseline(plan_name=>'&plan_name'));
+FROM   TABLE(DBMS_XPLAN.DISPLAY_SQL_PLAN_BASELINE(plan_name=>'&plan_name'));
 
 SET SERVEROUTPUT ON
 DECLARE
   l_plans_altered PLS_INTEGER;
 BEGIN
-  l_plans_altered := DBMS_SPM.alter_sql_plan_baseline(sql_handle => '&sql_handle', plan_name => '&plan_name', attribute_name => 'FIXED', attribute_value => 'YES');
+  l_plans_altered := DBMS_SPM.ALTER_SQL_PLAN_BASELINE(sql_handle => '&sql_handle', plan_name => '&plan_name', attribute_name => 'FIXED', attribute_value => 'YES');
   DBMS_OUTPUT.put_line('Plans Altered: ' || l_plans_altered);
 END;
 /
@@ -34,7 +34,7 @@ SET SERVEROUTPUT ON
 DECLARE
   l_plans_dropped  PLS_INTEGER;
 BEGIN
-  l_plans_dropped := DBMS_SPM.drop_sql_plan_baseline(sql_handle => '&sql_handle', plan_name  => '&plan_name');
-  DBMS_OUTPUT.put_line('Plans Dropped: ' ||l_plans_dropped);
+  l_plans_dropped := DBMS_SPM.DROP_SQL_PLAN_BASELINE(sql_handle => '&sql_handle', plan_name  => '&plan_name');
+  DBMS_OUTPUT.PUT_LINE('Plans Dropped: ' ||l_plans_dropped);
 END;
 /
