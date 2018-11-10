dc489897deb5d21a31d4bee3c1258813bd8563a4
Nagendra Chillale <nchillal@apple.com>
2018-11-10 13:39:31 -0800

    Changes

--- db_tables/constraints.sql
+++ db_tables/constraints.sql
@@ -1,36 +1,30 @@
-SET LINESIZE 155 PAGESIZE 200 PAUSE ON
-SET PAUSE "Press ENTER to continue . . . "
-ACCEPT TNAME PROMPT 'Enter Table Name: '
-ACCEPT SNAME PROMPT 'Enter Schema Name: '
-
-COLUMN owner FORMAT a20
-COLUMN column_name FORMAT a20
-COLUMN search_condition FORMAT a35
-COLUMN constraint_name FORMAT a30
-
-SELECT    constraint_name,
-          DECODE(constraint_type, 'C', 'Check',
-                                  'P', 'Primary',
-                                  'U', 'Unique',
-                                  'R', 'Referential') "Constraint Type",
-          search_condition,
-          status,
-          deferrable,
-          deferred,
-          validated,
-          generated
-FROM      dba_constraints
-WHERE     table_name = '&TNAME'
-AND       owner = '&SNAME'
-/
-
-SET PAUSE OFF
-ACCEPT CNAME PROMPT 'Enter Column Name: '
-
-SELECT    b.owner, a.constraint_name, b.table_name, a.column_name, b.search_condition, b.validated
-FROM      dba_cons_columns a, dba_constraints b
-WHERE     a.constraint_name = b.constraint_name
-AND       b.table_name = '&TNAME'
-AND       b.owner = '&SNAME'
-AND       a.column_name LIKE '&CNAME'
+DECLARE
+    -- Create bind variable for storing table_name
+    v_table_owner VARCHAR2(30) := '&table_owner';
+    v_table_name VARCHAR2(30) := '&table_name';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(CHR(13));
+    DBMS_OUTPUT.PUT_LINE(LPAD('*', 158, '*'));
+    DBMS_OUTPUT.PUT_LINE(RPAD('constraint_name', 35)||RPAD('column_name', 18)||RPAD('constraint_type', 18)||RPAD('status', 10)||RPAD('deferrable', 18)||RPAD('deferred', 12)||RPAD('validated', 12)||RPAD('search_condition', 35));
+    DBMS_OUTPUT.PUT_LINE(LPAD('*', 158, '*'));
+    FOR row in (SELECT    b.constraint_name constraint_name,
+                          a.column_name column_name,
+                          DECODE(b.constraint_type, 'C', 'Check',
+                                      'P', 'Primary',
+                                      'U', 'Unique',
+                                      'R', 'Referential') constraint_type,
+                          b.status status,
+                          b.deferrable deferrable,
+                          b.deferred deferred,
+                          b.validated validated,
+                          b.search_condition search_condition
+              FROM        dba_cons_columns a, dba_constraints b
+              WHERE       a.constraint_name = b.constraint_name
+              AND         b.table_name = v_table_name
+              AND         b.owner = v_table_owner
+    )
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(RPAD(row.constraint_name, 35)||RPAD(row.column_name, 18)||RPAD(row.constraint_type, 18)||RPAD(row.status, 10)||RPAD(row.deferrable, 18)||RPAD(row.deferred, 12)||RPAD(row.validated, 12)||RPAD(row.search_condition, 35));
+    END LOOP;
+END;
 /
