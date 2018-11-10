43ea9ba9d997330f144d162b58ce85b1fe8e5772
Nagendra Chillale <nchillal@apple.com>
2018-11-10 12:16:04 -0800

    Changes

--- getddl.sql
+++ getddl.sql
@@ -1,10 +1,10 @@
 BEGIN
-  dbms_metadata.set_transform_param(dbms_metadata.session_transform,'PRETTY',TRUE);
-  dbms_metadata.set_transform_param(dbms_metadata.session_transform,'SQLTERMINATOR',TRUE);
-  dbms_metadata.set_transform_param(dbms_metadata.session_transform,'CONSTRAINTS',TRUE);
-  dbms_metadata.set_transform_param(dbms_metadata.session_transform,'REF_CONSTRAINTS',TRUE);
-  dbms_metadata.set_transform_param(dbms_metadata.session_transform,'STORAGE', FALSE);
-  dbms_metadata.set_transform_param(dbms_metadata.session_transform,'TABLESPACE',TRUE);
+  dbms_metadata.set_transform_param(dbms_metadata.session_transform, 'PRETTY', TRUE);
+  dbms_metadata.set_transform_param(dbms_metadata.session_transform, 'SQLTERMINATOR', TRUE);
+  dbms_metadata.set_transform_param(dbms_metadata.session_transform, 'CONSTRAINTS', TRUE);
+  dbms_metadata.set_transform_param(dbms_metadata.session_transform, 'REF_CONSTRAINTS', TRUE);
+  dbms_metadata.set_transform_param(dbms_metadata.session_transform, 'STORAGE', FALSE);
+  dbms_metadata.set_transform_param(dbms_metadata.session_transform, 'TABLESPACE', TRUE);
 END;
 /
 
@@ -12,9 +12,10 @@ SET LONG 1000000
 SET LONGCHUNK 1000000
 SET LINESIZE 200
 SET PAGESIZE 0
-SELECT  dbms_metadata.get_ddl('&object_type','&object_name','&schema_name')
+SELECT  dbms_metadata.get_ddl('&object_type', '&object_name', '&schema_name')
 FROM    dual;
 
+-- Get tablespace ddl
 SELECT  dbms_metadata.get_ddl('TABLESPACE', tablespace_name)
 FROM    dba_tablespaces
 WHERE   tablespace_name = DECODE(UPPER('&&tablespace_name'), 'ALL', tablespace_name, UPPER('&&tablespace_name'));
