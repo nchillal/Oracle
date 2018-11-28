8562f340b26979efd46c3a720f5b95b4fb32fbbd
Nagendra Chillale <nchillal@apple.com>
2018-11-27 18:19:08 -0800

    Changes

--- db_performance/sqlid_baseline.sql
+++ db_performance/sqlid_baseline.sql
@@ -1,9 +1,10 @@
 BREAK ON sql_id SKIP 1
 
-SELECT  DISTINCT sql_id, child_number, sql_handle, plan_name, a.parsing_schema_name, origin, enabled, accepted, created, last_modified
+SELECT  DISTINCT sql_id, child_number, sql_handle, plan_name, a.parsing_schema_name, origin, enabled, accepted, created, last_modified, a.elapsed_time
 FROM    dba_sql_plan_baselines a, v$sql b
 WHERE   a.signature = b.exact_matching_signature
-AND     a.signature = b.force_matching_signature;
+AND     a.signature = b.force_matching_signature
+AND     b.sql_id = '&sql_id';
 
 
 
