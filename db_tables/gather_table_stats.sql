94c0ff127936d1f094b1707ccfa69ddd7088d98f
Nagendra Chillale <nchillal@apple.com>
2018-12-02 14:04:49 -0800

    Cascade option

--- db_tables/gather_table_stats.sql
+++ db_tables/gather_table_stats.sql
@@ -2,4 +2,4 @@ SELECT DBMS_STATS.get_prefs('&pname', '&schema', '&table') FROM dual;
 
 EXEC DBMS_STATS.set_table_prefs('&schema', '&table', '&pname', '&value');
 
-EXEC DBMS_STATS.gather_table_stats('&owner', '&table', estimate_percent => &est_percent);
+EXEC DBMS_STATS.gather_table_stats('&owner', '&table', estimate_percent => &est_percent, cascade => TRUE);
