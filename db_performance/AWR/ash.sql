4fad41e2e05ebbf83437ed02d77ba25a941f0dac
Nagendra Chillale <nchillal@apple.com>
2019-01-13 19:27:50 -0800

    Added sql_child_number column to the query

--- db_performance/AWR/ash.sql
+++ db_performance/AWR/ash.sql
@@ -2,12 +2,12 @@ BREAK ON sql_id ON sql_plan_hash_value SKIP 1
 COLUMN  event FORMAT a40
 SELECT  *
 FROM    (
-        SELECT    session_id, NVL(event, 'ON CPU') event, session_state, username, sql_id, sql_plan_hash_value, COUNT(*)
+        SELECT    session_id, NVL(event, 'ON CPU') event, session_state, username, sql_id, sql_child_number, sql_plan_hash_value, COUNT(*)
         FROM      v$active_session_history ash, dba_users du
         WHERE     ash.user_id=du.user_id
         AND       sample_time > SYSDATE - interval '&mins' minute
         AND       sql_id IS NOT NULL
-        GROUP BY  session_id, event, session_state, username, sql_id, sql_plan_hash_value
+        GROUP BY  session_id, event, session_state, username, sql_id, sql_child_number, sql_plan_hash_value
         ORDER BY  7 DESC
         )
 WHERE   rownum < 11;
@@ -17,6 +17,7 @@ FROM    (
         SELECT    session_id,
                   username,
                   sql_id,
+                  sql_child_number,
                   SUM(DECODE(session_state,'ON CPU',1,0)) AS CPU,
                   SUM(DECODE(session_state,'WAITING',1,0)) - SUM(DECODE(session_state,'WAITING', DECODE(wait_class, 'User I/O',1,0),0)) AS WAIT,
                   SUM(DECODE(session_state,'WAITING', DECODE(wait_class, 'User I/O',1,0),0)) AS IO,
@@ -25,7 +26,7 @@ FROM    (
         where     ash.user_id=du.user_id
         AND       sample_time > SYSDATE - interval '&mins' minute
         AND       sql_id IS NOT NULL
-        GROUP BY  session_id, username, sql_id
+        GROUP BY  session_id, username, sql_id, sql_child_number
         ORDER BY  SUM(DECODE(session_state,'ON CPU',1,1)) DESC
 )
 WHERE   ROWNUM <11;
