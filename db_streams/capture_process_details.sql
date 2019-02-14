ad0558d2f67963df6d6e6dff901fe1ed708d597b
Nagendra Chillale <nchillal@apple.com>
2019-02-13 20:44:51 -0800

    Adding capture process details script

---
+++ db_streams/capture_process_details.sql
@@ -0,0 +1,29 @@
+COLUMN action heading 'Capture Process Component' FORMAT A30
+COLUMN sid heading 'Session ID' FORMAT 999999
+COLUMN serial# heading 'Session|Serial|Number' FORMAT 99999999
+COLUMN process heading 'Operating System|Process Number' FORMAT A30
+COLUMN process_name heading 'Process|Name' FORMAT A20
+COLUMN total_messages_captured HEADING 'Redo|Entries|Evaluated|In Detail' FORMAT 999999999999
+COLUMN total_messages_enqueued HEADING 'Total|LCRs|Enqueued' FORMAT 999999999999
+COLUMN state HEADING 'State' FORMAT A30
+
+SELECT  /*+PARAM('_module_action_old_length',0)*/ action,
+        sid,
+        serial#,
+        sql_id,
+        process,
+        SUBSTR(program,INSTR(program,'(')+1,4) process_name
+FROM    v$session
+WHERE   module ='Streams'
+AND     action LIKE '%Capture%';
+
+SELECT  c.capture_name,
+        SUBSTR(s.program,INSTR(s.program,'(')+1,4) process_name,
+        c.sid,
+        c.serial#,
+        c.state,
+        c.total_messages_captured,
+        c.total_messages_enqueued
+FROM    v$streams_capture c, v$session s
+WHERE   c.sid = s.sid AND
+        c.serial# = s.serial#;
