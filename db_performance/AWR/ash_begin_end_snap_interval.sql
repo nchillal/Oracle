4272373e3f0d2ed921571e94bab00f1c7dbb172d
Nagendra Chillale <nagendra.chillale@yahoo.com>
2019-03-09 20:06:25 -0800

    Filter based on hour.

--- db_performance/AWR/ash_begin_end_snap_interval.sql
+++ db_performance/AWR/ash_begin_end_snap_interval.sql
@@ -11,3 +11,17 @@ FROM
           WHERE   instance_number = 1
           AND     begin_interval_time > SYSDATE - INTERVAL '&days' DAY
 );
+
+COLUMN begin_end_snapid FORMAT a20
+SELECT    begin_snap||' ---> '||end_snap "BEGIN_END_SNAPID",
+          begin_time||' ---> '||end_time "BEGIN_END_SNAPTIME"
+FROM
+(
+          SELECT  snap_id "BEGIN_SNAP",
+                  LEAD(SNAP_ID, 1) OVER(ORDER BY snap_id) "END_SNAP",
+                  TO_CHAR(BEGIN_INTERVAL_TIME, 'DDMMRRHH24MI') "BEGIN_TIME",
+                  LEAD(TO_CHAR(BEGIN_INTERVAL_TIME, 'DDMMRRHH24MI'), 1) OVER(ORDER BY snap_id) "END_TIME"
+          FROM    dba_hist_snapshot
+          WHERE   instance_number = 1
+          AND     begin_interval_time > SYSDATE - INTERVAL '&hours' HOUR
+);
