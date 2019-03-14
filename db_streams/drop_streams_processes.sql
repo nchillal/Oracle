f9433009f60da0c80e9da29200d85068e0af5400
Nagendra Chillale <nagendra.chillale@yahoo.com>
2019-03-14 07:41:52 -0700

    Changes

--- db_streams/drop_streams_processes.sql
+++ db_streams/drop_streams_processes.sql
@@ -1,8 +1,8 @@
 -- Drop streams capture process.
-EXEC DBMS_CAPTURE_ADM.DROP_CAPTURE ( capture_name => '&capture_name', drop_unused_rule_sets => true);
+EXEC DBMS_CAPTURE_ADM.DROP_CAPTURE (capture_name => '&capture_name', drop_unused_rule_sets => true);
 -- Drop streams propagation process.
 EXEC DBMS_PROPAGATION_ADM.DROP_PROPAGATION(propagation_name => '&propagation_name', drop_unused_rule_sets => true);
 -- Drop streams apply process.
 EXEC DBMS_APPLY_ADM.DROP_APPLY(apply_name => '&apply_name', drop_unused_rule_sets => true);
 -- Drop streams queue
-EXEC DBMS_STREAMS_ADM.REMOVE_QUEUE ( queue_name => '&queue_name', cascade => FALSE, drop_unused_queue_table => TRUE );
+EXEC DBMS_STREAMS_ADM.REMOVE_QUEUE(queue_name => '&queue_name', cascade => FALSE, drop_unused_queue_table => TRUE);
