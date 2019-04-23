bf9cc41d2e7bbccaeecc963edfc1d876b3f9f835
Nagendra Chillale <nagendra.chillale@yahoo.com>
2019-04-23 08:23:01 -0700

    Converted tabs to space

--- db_logfiles/logfile_switch.sql
+++ db_logfiles/logfile_switch.sql
@@ -1,17 +1,17 @@
 SET SERVEROUTPUT ON
 DECLARE
-	v_arch_log_idle_mins_threshold NUMBER:=25;
-	v_arch_log_idle_mins NUMBER;
+    v_arch_log_idle_mins_threshold NUMBER:=25;
+    v_arch_log_idle_mins NUMBER;
 BEGIN
-	SELECT 	(SYSDATE-COMPLETION_TIME)*24*60 INTO v_arch_log_idle_mins
-	FROM 		v$archived_log
-	WHERE 	sequence# = (SELECT MAX(sequence#) FROM v$log_history) AND dest_id=1;
+    SELECT   (SYSDATE-COMPLETION_TIME)*24*60 INTO v_arch_log_idle_mins
+    FROM     v$archived_log
+    WHERE    sequence# = (SELECT MAX(sequence#) FROM v$log_history) AND dest_id=1;
 
-	IF v_arch_log_idle_mins > v_arch_log_idle_mins_threshold THEN
-		DBMS_OUTPUT.PUT_LINE(CHR(10)||'Archive log idle minutes is '||v_arch_log_idle_mins||' which is above threshold '||v_arch_log_idle_mins_threshold);
-		EXECUTE IMMEDIATE 'ALTER SYSTEM SWITCH LOGFILE';
-	ELSE
-		DBMS_OUTPUT.PUT_LINE(CHR(10)||'Archive log idle minutes is '||v_arch_log_idle_mins||' which is within threshold '||v_arch_log_idle_mins_threshold);
-	END IF;
+    IF v_arch_log_idle_mins > v_arch_log_idle_mins_threshold THEN
+        DBMS_OUTPUT.PUT_LINE(CHR(10)||'Archive log idle minutes is '||v_arch_log_idle_mins||' which is above threshold '||v_arch_log_idle_mins_threshold);
+        EXECUTE IMMEDIATE 'ALTER SYSTEM SWITCH LOGFILE';
+    ELSE
+        DBMS_OUTPUT.PUT_LINE(CHR(10)||'Archive log idle minutes is '||v_arch_log_idle_mins||' which is within threshold '||v_arch_log_idle_mins_threshold);
+    END IF;
 END;
 /
--- db_patching/dba_registry.sql
+++ db_patching/dba_registry.sql
@@ -1,10 +1,10 @@
 SET linesize 230 pagesize 30
 COLUMN comp_name FORMAT a80
-SELECT    comp_id,
-          comp_name,
-          version,
-          status
-FROM      dba_registry
+SELECT  comp_id,
+        comp_name,
+        version,
+        status
+FROM    dba_registry
 /
 
 COLUMN description FORMAT a55
@@ -12,12 +12,12 @@ COLUMN action_time FORMAT a30
 COLUMN status FORMAT a10
 COLUMN action FORMAT a10
 
-SELECT    patch_id,
-          patch_type,
-          action,
-          status,
-          action_time,
-          description,
-          source_version,
-          target_version
-FROM      sys.dba_registry_sqlpatch;
+SELECT  patch_id,
+        patch_type,
+        action,
+        status,
+        action_time,
+        description,
+        source_version,
+        target_version
+FROM    sys.dba_registry_sqlpatch;
