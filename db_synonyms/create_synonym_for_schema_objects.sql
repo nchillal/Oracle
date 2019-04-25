1bed7023e9a5c5c5509196382f09a86c80647c51
Nagendra Chillale <nagendra.chillale@yahoo.com>
2019-04-24 20:49:58 -0700

    Added or replace

--- db_synonyms/create_synonym_for_schema_objects.sql
+++ db_synonyms/create_synonym_for_schema_objects.sql
@@ -1,11 +1,11 @@
 SET LINESIZE 250 ECHO OFF PAGESIZE 2000
 
 -- Public Synonym Creation For Tables
-SELECT  'CREATE PUBLIC SYNONYM '||table_name||' FOR '||owner||'.'||table_name||';'
+SELECT  'CREATE OR REPLACE PUBLIC SYNONYM '||table_name||' FOR '||owner||'.'||table_name||';'
 FROM    dba_tables
 WHERE   owner='&&SCHEMA';
 
 -- Public Synonym Creation For Sequences
-SELECT  'CREATE PUBLIC SYNONYM '||sequence_name||' FOR '||sequence_owner||'.'||sequence_name||';'
+SELECT  'CREATE OR REPLACE PUBLIC SYNONYM '||sequence_name||' FOR '||sequence_owner||'.'||sequence_name||';'
 FROM    dba_sequences
 WHERE   sequence_owner='&&SCHEMA';
