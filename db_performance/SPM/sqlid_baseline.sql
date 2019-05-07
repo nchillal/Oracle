39e37d139d1df2d7798bbb93b6f6656cc6f5d2b1
Nagendra Chillale <nagendra.chillale@yahoo.com>
2019-05-07 15:40:22 -0700

    Added fixed column

--- db_performance/SPM/sqlid_baseline.sql
+++ db_performance/SPM/sqlid_baseline.sql
@@ -1,6 +1,6 @@
 BREAK ON sql_id SKIP 1
 
-SELECT  DISTINCT sql_id, child_number, sql_handle, plan_name, a.parsing_schema_name, origin, enabled, accepted, created, last_modified, a.elapsed_time
+SELECT  DISTINCT sql_id, child_number, sql_handle, plan_name, a.parsing_schema_name, origin, enabled, accepted, created, fixed, last_modified, a.elapsed_time
 FROM    dba_sql_plan_baselines a, v$sql b
 WHERE   a.signature = b.exact_matching_signature
 AND     a.signature = b.force_matching_signature
