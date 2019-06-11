e0a4d7e7c979c584d36fa155422ccdcf9d0a999b
Nagendra Chillale <nchillalemohan@nagendras-mbp-2.attlocal.net>
2019-06-10 19:16:17 -0700

    Changes

--- directories.sql
+++ directories.sql
@@ -2,3 +2,8 @@ SET LINESIZE 230 PAGESIZE 200
 COLUMN directory_path FORMAT a80
 SELECT  *
 FROM    dba_directories;
+
+SELECT      directory_name, grantee, privilege
+FROM        dba_tab_privs t, all_directories d
+WHERE       t.table_name(+)=d.directory_name
+ORDER BY    1,2,3;
