668aee986ee5eb2ad22fe0c71c2b7d565361913e
Nagendra Chillale <nchillalemohan@nagendras-mbp-2.attlocal.net>
2019-06-10 19:16:54 -0700

    Changes

--- directories.sql
+++ directories.sql
@@ -3,6 +3,8 @@ COLUMN directory_path FORMAT a80
 SELECT  *
 FROM    dba_directories;
 
+BREAK ON directory_name ON grantee SKIP 1
+
 SELECT      directory_name, grantee, privilege
 FROM        dba_tab_privs t, all_directories d
 WHERE       t.table_name(+)=d.directory_name
