c02ef061997462eb980e720bed61aaeb7a48f2fb
Nagendra Chillale <nchillalemohan@nagendras-mbp-2.attlocal.net>
2019-07-21 10:15:56 -0700

    Added code to get from AWR

--- temp_tbs/temp_usage.sql
+++ temp_tbs/temp_usage.sql
@@ -11,31 +11,24 @@ COLUMN statements FORMAT 999
 COLUMN hash_value FORMAT 99999999999
 COLUMN sql_text FORMAT a50
 COLUMN service_name FORMAT a15
-prompt
-prompt #####################################################################
-prompt #######################LOCAL TEMP USERS#############################
-prompt #####################################################################
-prompt
-SELECT    S.sid || ',' || S.serial# sid_serial, S.username, S.osuser, P.spid, S.module, P.program, s.service_name,
-          SUM (T.blocks) * TBS.block_size / 1024 / 1024 mb_used, T.tablespace,
+
+SELECT    s.sid||','||s.serial# sid_serial, 
+          s.username, s.osuser, p.spid, 
+          s.module,
+          SUM(t.blocks)*tbs.block_size/1024/1024 mb_used, T.tablespace,
           COUNT(*) statements
-FROM      v$tempseg_usage T, v$session S, dba_tablespaces TBS, v$process P
+FROM      v$tempseg_usage t, v$session s, dba_tablespaces tbs, v$process p
 WHERE     T.session_addr = S.saddr
 AND       S.paddr = P.addr
-AND       T.tablespace = TBS.tablespace_name
-GROUP BY  S.sid, S.serial#, S.username, S.osuser, P.spid, S.module, P.program, s.service_name, TBS.block_size, T.tablespace
+AND       T.tablespace = tbs.tablespace_name
+GROUP BY  S.sid, S.serial#, S.username, S.osuser, P.spid, S.module, P.program, s.service_name, tbs.block_size, T.tablespace
 ORDER BY  mb_used;
-prompt
-prompt #####################################################################
-prompt #######################LOCAL ACTIVE SQLS ############################
-prompt #####################################################################
-prompt
+
 SELECT    sysdate "TIME_STAMP",
           vsu.username,
           vs.sid,
           vp.spid,
           vs.sql_id,
-          vst.sql_text,
           vsu.segtype,
           vsu.tablespace,
           vs.service_name,
@@ -57,18 +50,13 @@ AND       vst.piece = 0
 AND       vs.status='ACTIVE'
 AND       dt.tablespace_name = vsu.tablespace
 ORDER BY  usage_mb;
-prompt
-prompt #####################################################################
-prompt #######################LOCAL TEMP SQLS##############################
-prompt #####################################################################
-prompt
-SELECT    s.sid ||','|| s.serial# sid_serial, s.username, q.sql_id, q.sql_text, t.blocks * TBS.block_size / 1024 / 1024 mb_used, t.tablespace
+
+SELECT    s.sid ||','|| s.serial# sid_serial, s.username, q.sql_id, t.blocks * tbs.block_size / 1024 / 1024 mb_used, t.tablespace
 FROM      v$tempseg_usage t, v$session s, v$sqlarea q, dba_tablespaces tbs
 WHERE     t.session_addr = s.saddr
 AND       t.sqladdr = q.address
 AND       t.tablespace = tbs.tablespace_name
 ORDER BY  mb_used;
-
 
 BREAK ON snap_id SKIP 1
 SELECT    snap_id,
@@ -90,3 +78,61 @@ PIVOT     (
           FOR metric_name IN ('Temp Space Used' AS TEMP_SPACE_USED)
           )
 ORDER BY  snap_id;
+
+COLUMN sample_time FORMAT a30
+SELECT      sample_time,
+            session_id,
+            session_serial#,
+            sql_id,
+            sql_plan_hash_value,
+            temp_space_allocated/1024/1024 temp_mb,
+            temp_space_allocated/1024/1024 - LAG(temp_space_allocated/1024/1024,1,0) OVER (ORDER BY sample_time) AS temp_diff
+FROM        v$active_session_history
+WHERE       sample_time > TO_DATE('&&start_datetime','DDMMRRHH24MI')
+AND         sample_time < TO_DATE('&&end_datetime','DDMMRRHH24MI')
+AND         temp_space_allocated/1024/1024 > 100
+ORDER BY    temp_mb ASC
+/
+
+COLUMN sample_time FORMAT a30
+SELECT      sample_time,
+            session_id,
+            session_serial#,
+            sql_id,
+            sql_plan_hash_value,
+            temp_space_allocated/1024/1024 temp_mb,
+            temp_space_allocated/1024/1024 - LAG(temp_space_allocated/1024/1024,1,0) OVER (ORDER BY sample_time) AS temp_diff
+FROM        dba_hist_active_sess_history
+WHERE       snap_id BETWEEN &&begin_snap AND &&end_snap
+AND         temp_space_allocated/1024/1024 > 100
+ORDER BY    temp_mb ASC
+/
+
+COLUMN sample_time FORMAT a30
+SELECT      sample_time,
+            session_id,
+            session_serial#,
+            machine,
+            temp_space_allocated/1024/1024 temp_mb,
+            temp_space_allocated/1024/1024 - LAG(temp_space_allocated/1024/1024,1,0) OVER (ORDER BY sample_time) AS temp_diff
+FROM        v$active_session_history
+WHERE       sample_time > TO_DATE('&&start_datetime','DDMMRRHH24MI')
+AND         sample_time < TO_DATE('&&end_datetime','DDMMRRHH24MI')
+AND         sql_id = '&sql_id'
+AND         temp_space_allocated/1024/1024 > 100
+ORDER BY    temp_mb ASC
+/
+
+COLUMN sample_time FORMAT a30
+SELECT      sample_time,
+            session_id,
+            session_serial#,
+            machine,
+            temp_space_allocated/1024/1024 temp_mb,
+            temp_space_allocated/1024/1024 - LAG(temp_space_allocated/1024/1024,1,0) OVER (ORDER BY sample_time) AS temp_diff
+FROM        dba_hist_active_sess_history
+WHERE       snap_id BETWEEN &&begin_snap AND &&end_snap
+AND         sql_id = '&sql_id'
+AND         temp_space_allocated/1024/1024 > 100
+ORDER BY    temp_mb ASC
+/
