29b532720bcb7aa7edcd80a6ac5adc8b73757950
Nagendra Chillale <nchillalemohan@nagendras-mbp-2.attlocal.net>
2019-07-21 10:14:38 -0700

    Fixed syntax issue

--- db_sequence/gen_create_sequence.sql
+++ db_sequence/gen_create_sequence.sql
@@ -1,7 +1,7 @@
-SELECT  'CREATE SEQUENCES '||sequence_owner||'.'||sequence_name||' START WITH '||last_number||' INCREMENT BY '||increment_by||';'
+SELECT  'CREATE SEQUENCE '||sequence_owner||'.'||sequence_name||' START WITH '||last_number||' INCREMENT BY '||increment_by||';'
 FROM    dba_sequences
 WHERE   sequence_owner = '&owner';
 
-SELECT  'CREATE SEQUENCES '||sequence_owner||'.'||sequence_name||' START WITH '||last_number||' INCREMENT BY '||increment_by||';'
+SELECT  'CREATE SEQUENCE '||sequence_owner||'.'||sequence_name||' START WITH '||last_number||' INCREMENT BY '||increment_by||';'
 FROM    dba_sequences
 WHERE   sequence_name = '&name';
