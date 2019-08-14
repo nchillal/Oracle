f86d5d98415113da0b4cc54ddad66f277cfad122
Nagendra Chillale <nchillal@apple.com>
2019-08-14 14:06:14 -0700

    Added dbid

--- db_performance/AWR/awr_settings.sql
+++ db_performance/AWR/awr_settings.sql
@@ -1,3 +1,4 @@
-SELECT  EXTRACT(day FROM snap_interval) * 24 * 60 + EXTRACT(hour FROM snap_interval) * 60 + EXTRACT(minute FROM snap_interval) "Snapshot Interval",
+SELECT  dbid,
+        EXTRACT(day FROM snap_interval) * 24 * 60 + EXTRACT(hour FROM snap_interval) * 60 + EXTRACT(minute FROM snap_interval) "Snapshot Interval",
         EXTRACT(day FROM retention) * 24 * 60 + EXTRACT(hour FROM retention) * 60 + EXTRACT(minute FROM retention) "Retention Interval"
 FROM    dba_hist_wr_control;
