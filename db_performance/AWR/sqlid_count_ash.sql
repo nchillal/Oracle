72ca532bc4fd29beaf9a7a549eb5b1e08e8f8ad4
Nagendra Chillale <nchillal@apple.com>
2019-08-20 15:03:42 -0700

    Changes

--- db_performance/AWR/sqlid_count_ash.sql
+++ db_performance/AWR/sqlid_count_ash.sql
@@ -22,3 +22,13 @@ FROM    (
         ORDER BY  COUNT(*) DESC
         )
 WHERE   rownum <= 10;
+
+BREAK ON sql_id ON sql_child_number ON username SKIP 1
+SELECT      username, sql_id, sql_child_number, sample_time, COUNT(*)
+FROM        v$active_session_history ash, dba_users du
+WHERE       ash.user_id = du.user_id
+AND         ash.user_id > 0
+AND         sql_id = '&sql_id'
+AND         sql_child_number = '&sql_child_number'
+GROUP BY    username, sample_time, sql_id, sql_child_number
+ORDER BY    sample_time;
