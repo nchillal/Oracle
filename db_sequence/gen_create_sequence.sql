e4bd20c47773e24e2116c674e4f11d4df9338830
Nagendra Chillale <nchillal@apple.com>
2019-10-01 19:40:10 -0700

    Changes

--- db_sequence/gen_create_sequence.sql
+++ db_sequence/gen_create_sequence.sql
@@ -1,7 +1,9 @@
-SELECT  'CREATE SEQUENCE '||sequence_owner||'.'||sequence_name||' START WITH '||last_number||' INCREMENT BY '||increment_by||';'
+SELECT  'CREATE SEQUENCE '||sequence_owner||'.'||sequence_name||' MINVALUE '||MIN_VALUE||' MAXVALUE '||MAX_VALUE||' START WITH '||last_number||' INCREMENT BY '||increment_by||';'
 FROM    dba_sequences
-WHERE   sequence_owner = '&owner';
+WHERE   sequence_owner = '&owner'
+AND     sequence_name = '&sequence_name';
 
-SELECT  'CREATE SEQUENCE '||sequence_owner||'.'||sequence_name||' START WITH '||last_number||' INCREMENT BY '||increment_by||';'
+SELECT  'CREATE SEQUENCE '||sequence_owner||'.'||sequence_name||' START WITH '||last_number||' INCREMENT BY '||increment_by||';' AS SEQUENCE_SQL
 FROM    dba_sequences
-WHERE   sequence_name = '&name';
+WHERE   sequence_name = '&name'
+AND     sequence_name = '&sequence_name';
--- logminer.sql
+++ logminer.sql
@@ -5,7 +5,9 @@
 
 SET LINESIZE 250 PAGES 2000
 COLUMN name FORMAT a70
-SELECT first_time, name FROM v$archived_log WHERE dest_id=1 AND first_time BETWEEN '&start_date' AND '&end_date'  ORDER BY 1;
+SELECT first_time, name FROM v$archived_log WHERE dest_id=1 AND first_time > SYSDATE - INTERVAL '&days' DAY;
+SELECT first_time, name FROM v$archived_log WHERE dest_id=1 AND first_time > SYSDATE - INTERVAL '&hours' HOUR;
+SELECT first_time, name FROM v$archived_log WHERE dest_id=1 AND first_time > SYSDATE - INTERVAL '&minutes' MINUTE;
 SELECT first_time, name FROM v$archived_log WHERE dest_id=1 AND sequence# BETWEEN &begin_sequence AND &end_sequence ORDER BY 1;
 
 -- Add initial archive logfile to be mined.
@@ -18,7 +20,7 @@ EXEC DBMS_LOGMNR.ADD_LOGFILE (options => DBMS_LOGMNR.ADDFILE, logfilename => '&a
 BEGIN
     DBMS_OUTPUT.PUT_LINE(chr(13));
     FOR rec in  (
-                SELECT first_time, name FROM v$archived_log WHERE dest_id=1 AND first_time BETWEEN '&start_date' AND '&end_date'  ORDER BY 1
+                SELECT first_time, name FROM v$archived_log WHERE dest_id=1 AND first_time BETWEEN '&start_date' AND '&end_date' ORDER BY 1
                 )
     LOOP
         DBMS_OUTPUT.PUT_LINE('Adding archive logfile '||rec.name||' for logminer to mine');
@@ -50,10 +52,14 @@ BEGIN
 END;
 /
 
-SELECT  (xidusn || '.' || xidslt || '.' || xidsqn) AS xid, username, sql_redo
-FROM    v$logmnr_contents
-WHERE   username != 'SYS'
-AND     seg_owner IS NULL OR seg_owner NOT IN ('SYS', 'SYSTEM');
+COLUMN sql_redo FORMAT a100
+COLUMN xid FORMAT a15
+
+SELECT      start_timestamp, commit_timestamp, username, sql_redo
+FROM        v$logmnr_contents
+WHERE       username != 'SYS'
+AND         seg_owner IS NULL OR seg_owner NOT IN ('SYS', 'SYSTEM')
+ORDER BY    1;
 
 EXECUTE DBMS_LOGMNR.END_LOGMNR();
 
