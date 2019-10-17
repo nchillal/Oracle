85a2a992eabff9248ab8a70388bb30bc3e796d69
Nagendra Chillale <nchillal@apple.com>
2019-10-16 18:41:19 -0700

    Changes

--- db_sequence/gen_public_synonyms.sql
+++ db_sequence/gen_public_synonyms.sql
@@ -2,12 +2,12 @@ SET LINES 150 PAGES 2000
 
 SELECT  owner, synonym_name, table_owner, table_name
 FROM    dba_synonyms
-WHERE   synonym_name IN (SELECT sequence_name FROM dba_sequences);
+WHERE   synonym_name IN (SELECT sequence_name FROM dba_sequences WHERE sequence_owner='&owner');
 
 SET HEADING OFF
 
 SELECT  'CREATE '||owner||' SYNONYM '||synonym_name||' FOR '||table_owner||'.'||table_name||';'
 FROM    dba_synonyms
-WHERE   synonym_name IN (SELECT sequence_name FROM dba_sequences WHERE sequence_owner='&owner');
+WHERE   synonym_name IN (SELECT sequence_name FROM dba_sequences WHERE sequence_owner='&owner' AND sequence_name = '&sequence_name');
 
 SET HEADING ON
