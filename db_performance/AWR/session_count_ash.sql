9a23e4070d662bfbb5e5e393034a375edf98bd39
Nagendra Chillale <nchillal@apple.com>
2020-03-21 21:53:31 -0700

    Changes

--- db_performance/AWR/session_count_ash.sql
+++ db_performance/AWR/session_count_ash.sql
@@ -18,3 +18,21 @@ AND       du.user_id <> 0
 AND       snap_id BETWEEN '&&begin_snap' AND '&&end_snap'
 GROUP BY  dash.instance_number, du.username, TO_CHAR(sample_time,'MON-DD-YYYY'), TO_CHAR(sample_time,'HH24:MI')
 ORDER BY  1,2,3;
+
+SELECT username, TO_CHAR(sample_time,'YYYY-MM-DD') DAY, COUNT(*) CNT
+FROM
+(
+    SELECT    du.username username, sample_time
+    FROM      gv$active_session_history ash, dba_users du
+    WHERE     ash.user_id = du.user_id
+    AND       du.user_id <> 0
+    AND       du.username = '&&username'
+    UNION
+    SELECT    du.username username, sample_time
+    FROM      dba_hist_active_sess_history dash, dba_users du
+    WHERE     dash.user_id = du.user_id
+    AND       du.user_id <> 0
+    AND       du.username = '&&username'
+)
+GROUP BY username, TO_CHAR(sample_time,'YYYY-MM-DD')
+ORDER BY 2;
